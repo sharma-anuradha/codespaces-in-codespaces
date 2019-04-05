003679e7e63f924118e33843c502f35dee924e55
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2019-04-05 07:27:01 -0700

    Update .net core versions

--- cdpx-build.cmd
+++ cdpx-build.cmd
@@ -9,7 +9,7 @@ echo Building %~nx0
 pushd "%~dp0"
 
 :: Install dotnet
-set DOTNET_VERSION=2.2.103
+set DOTNET_VERSION=2.2.202
 call ".pipelines\install-dotnet.cmd" %DOTNET_VERSION%
 
 set DOTNET_ARGS=/m /v:m
