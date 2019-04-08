0f1ec4189b5786cf633e35b8df76426504869645
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2019-04-08 11:57:27 -0700

    Add echo endpoint to both services

--- src/services/lib/VsClk.Hosting/HostingProgramBase.cs
+++ src/services/lib/VsClk.Hosting/HostingProgramBase.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace Microsoft.VsCloudKernel.Services.VsClk.Hosting
 {
@@ -8,6 +14,40 @@ namespace Microsoft.VsCloudKernel.Services.VsClk.Hosting
     {
         public static void Run(string[] args)
         {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                // TEMP ENDPOINT
+                .Configure(app => app.Map("/echo", EchoHandler))
+                .Build();
+
+            host.Run();
+        }
+
+        private static void EchoHandler(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                context.Response.ContentType = context.Request.ContentType;
+                await context.Response.WriteAsync(
+                    JsonConvert.SerializeObject(new
+                    {
+                        StatusCode = context.Response.StatusCode.ToString(),
+                        PathBase = context.Request.PathBase.Value.Trim('/'),
+                        Path = context.Request.Path.Value.Trim('/'),
+                        Method = context.Request.Method,
+                        Scheme = context.Request.Scheme,
+                        ContentType = context.Request.ContentType,
+                        ContentLength = (long?)context.Request.ContentLength,
+                        Content = new StreamReader(context.Request.Body).ReadToEnd(),
+                        QueryString = context.Request.QueryString.ToString(),
+                        Query = context.Request.Query
+                            .ToDictionary(
+                                item => item.Key,
+                                item => item.Value,
+                                StringComparer.OrdinalIgnoreCase)
+                    })
+                );
+            });
         }
     }
 }
