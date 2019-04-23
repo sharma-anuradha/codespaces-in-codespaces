2d374ad875c49ba4a19b195eb31991423fcbeef8
Sam El-Husseini <samelh@microsoft.com>
2019-04-23 18:32:55 +0000

    Merged PR 177074: Remove reading settings from keyvault.
    Remove reading settings from keyvault.
    The hypothesis here is this is interfering with the rest of app settings as it tries to pull settings from keyvault in the app.
    Anthony added this in the App service world, but we never got it to work, removing it as it's not needed here and is likely interfering with our own way of fetching app settings

--- src/services/containers/VsClk.EnvReg.WebApi/Program.cs
+++ src/services/containers/VsClk.EnvReg.WebApi/Program.cs
@@ -25,30 +25,6 @@ namespace Microsoft.VsCloudKernel.Services.EnvReg.WebApi
                         .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true)
                         .AddJsonFile($"appsettings.secrets.json", optional: true)
                         .AddEnvironmentVariables(ServiceName + "_");    // ENVREG_AppSettings__*
-
-                    // Setup keyvault read
-                    if (context.HostingEnvironment.IsProduction())
-                    {
-                        var builtConfig = config.Build();
-
-                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                        var keyVaultClient = new KeyVaultClient(
-                            new KeyVaultClient.AuthenticationCallback(
-                                azureServiceTokenProvider.KeyVaultTokenCallback));
-
-                        var keyVaultName = builtConfig["KeyVaultName"];
-                        if (string.IsNullOrEmpty(keyVaultName))
-                        {
-                            // TEMPORARY fix to see if we can get through startup in Azure App Service
-                            keyVaultName = "vsclk-envreg-dev-kv";
-                        }
-                        var keyVaultUri = $"https://{keyVaultName}.vault.azure.net/";
-
-                        config.AddAzureKeyVault(
-                            keyVaultUri,
-                            keyVaultClient,
-                            new DefaultKeyVaultSecretManager());
-                    }
                 })
                 .UseStartup<Startup>();
     }
