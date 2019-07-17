91e6beb6b0f5f3f6a74e83aeb5c8540378e91a0c
Anuradha Sharma <anush@microsoft.com>
2019-07-17 21:29:17 +0000

    Merged PR 191918: Add Compute Provisioning Service Stub
    Add Compute Provisioning Service Stub
    
    Related work items: #942032

---
+++ src/services/containers/VsClk.ComProv.Worker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VsSaaS.Diagnostics;
+using Microsoft.VsSaaS.Diagnostics.Extensions;
+
+namespace VsClk.ComProv.Worker
+{
+    class Program
+    {
+        public static string ServiceName = "COMPROV";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Load configuration
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"appsettings.secrets.json", optional: true)
+               .AddEnvironmentVariables(ServiceName + "_");    // COMPROV_AppSettings__*
+            var configuration = builder.Build();
+
+            var loggerFactory = new DefaultLoggerFactory();
+            var logger = loggerFactory.New(new LogValueSet { { "Service", "ContainerPoolWorker" } });
+
+            logger.LogInfo("compute_provisioning_start");
+
+            var duration = logger.StartDuration();
+
+            #region vm_provisioning
+
+            #endregion vm_provisioning 
+
+            logger.AddDuration(duration);
+            logger.LogInfo("compute_provisioning_completed");
+        }
+    }
+}
