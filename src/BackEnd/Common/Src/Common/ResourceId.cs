5633a68d8ca94c8e9b1de0ad4b7672979bb8b7ec
John Rivard <johnri@microsoft.com>
2019-08-04 16:09:10 +0000

    Merged PR 195217: Back-end resource ids
    I got bored re-watching a movie and decided to code up back-end resource ids. We can debate whether this will be useful throughout the system, and what the final form should take.
    
    The `ResourceId` struct represents the quad { resource type, instance id, subscription id, location  }. The resource type is not to be confused with an azure resource type, but rather, a cloud environment resource type. Perhaps we can think up a better name to avoid downstream confusion.
    
    Both `InstanceId` and `SubscriptionId` are formally of type `Guid` to ensure consistent and valid formats. The `ResourceType` and `Locations` properties are enums to control for the valid set of values and for max string length of the `Id` string.
    
    The `Id` property and the `ToString` method return the canonical format that is useful for passing between the front-end and back-end, and for durable storage. The `Parse` and `TryParse` methods will render the struct from the formatted string.
    
    The Id format is
    `vssaas/resourcetypes/{resourcetype}/instances/{instanceid}/subscriptions/{subscriptionid}/locations/{locationid}`
    
    The struct support custom equality, hash code, == and != operators.

---
+++ src/BackEnd/Common/Src/Common/ResourceId.cs
@@ -0,0 +1,254 @@
+// <copyright file="ResourceId.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+using System;
+using System.Text.RegularExpressions;
+using Microsoft.VsSaaS.Services.CloudEnvironments.Common.Models;
+
+namespace Microsoft.VsSaaS.Services.CloudEnvironments.Common
+{
+    /// <summary>
+    /// A Cloud Environment back-end resource id.
+    /// </summary>
+    public struct ResourceId : IEquatable<ResourceId>
+    {
+        /// <summary>
+        /// The empty/blank/null/default resource id.
+        /// </summary>
+        public static readonly ResourceId Empty = default;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceId"/> struct.
+        /// </summary>
+        /// <param name="resourceType">The cloud environment resource type.</param>
+        /// <param name="instanceId">The resource name.</param>
+        /// <param name="subscriptionId">The azure subscription id.</param>
+        /// <param name="location">The azure location.</param>
+        public ResourceId(
+            ResourceType resourceType,
+            Guid instanceId,
+            Guid subscriptionId,
+            AzureLocation location)
+        {
+            // Construct an empty id.
+            if (resourceType == default &&
+                instanceId == default &&
+                subscriptionId == default &&
+                location == default)
+            {
+                SubscriptionId = default;
+                ResourceType = default;
+                InstanceId = default;
+                Location = default;
+                Id = null;
+            }
+            else
+            {
+                Requires.NotEmpty(subscriptionId, nameof(subscriptionId));
+                Requires.NotEmpty(instanceId, nameof(instanceId));
+
+                ResourceType = RequiresEnumIsDefined(resourceType, nameof(resourceType));
+                InstanceId = instanceId;
+                SubscriptionId = subscriptionId;
+                Location = RequiresEnumIsDefined(location, nameof(location));
+                Id = Parser.FormatId(ResourceType, InstanceId, SubscriptionId, Location);
+            }
+        }
+
+        /// <summary>
+        /// Gets the Cloud Environment resource type.
+        /// </summary>
+        public ResourceType ResourceType { get; }
+
+        /// <summary>
+        /// Gets the resource instance id.
+        /// </summary>
+        public Guid InstanceId { get; }
+
+        /// <summary>
+        /// Gets the azure subscription in which this resource has been created.
+        /// </summary>
+        public Guid SubscriptionId { get; }
+
+        /// <summary>
+        /// Gets the Azure resource location.
+        /// </summary>
+        public AzureLocation Location { get; }
+
+        /// <summary>
+        /// Gets the fully-qualified resource Id, which is valid for serialization and storage.
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// Returns <see cref="Id"/>.
+        /// </summary>
+        /// <param name="id">The resource id.</param>
+        public static implicit operator string(ResourceId id) => id.Id;
+
+        /// <summary>
+        /// Returns <see cref="ResourceType"/>.
+        /// </summary>
+        /// <param name="id">The resource id.</param>
+        public static implicit operator ResourceType(ResourceId id) => id.ResourceType;
+
+        /// <summary>
+        /// Returns <see cref="InstanceId"/>.
+        /// </summary>
+        /// <param name="id">The resource id.</param>
+        public static implicit operator Guid(ResourceId id) => id.InstanceId;
+
+        /// <summary>
+        /// Returns <see cref="Location"/>.
+        /// </summary>
+        /// <param name="id">The resource id.</param>
+        public static implicit operator AzureLocation(ResourceId id) => id.Location;
+
+        public static bool operator ==(ResourceId first, ResourceId second) => first.Equals(second);
+
+        public static bool operator !=(ResourceId first, ResourceId second) => !first.Equals(second);
+
+        /// <summary>
+        /// Parse a Cloud Environment resource id into a <see cref="ResourceId"/>.
+        /// </summary>
+        /// <param name="id">The resource id string.</param>
+        /// <param name="value">The output value.</param>
+        /// <returns>True if the value was valid, otherwise false.</returns>
+        public static bool TryParse(string id, out ResourceId value) => Parser.TryParse(id, out value, out _);
+
+        /// <summary>
+        /// Parse a Cloud Environment resoruce id into a <see cref="ResourceId"/>.
+        /// </summary>
+        /// <param name="id">The resource id string.</param>
+        /// <returns>The resource id structure.</returns>
+        public static ResourceId Parse(string id)
+        {
+            // Note: null and empty are valid inputs, resulting in ResourceId.Empty.
+            if (!Parser.TryParse(id, out var value, out var reason))
+            {
+                throw new FormatException(reason);
+            }
+
+            return value;
+        }
+
+        /// <inheritdoc/>
+        public override string ToString() => Id;
+
+        /// <inheritdoc/>
+        public bool Equals(ResourceId other) => string.Equals(Id, other.Id, StringComparison.OrdinalIgnoreCase);
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            if (obj is ResourceId resourceId)
+            {
+                return Equals(resourceId);
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode() => (Id?.GetHashCode()).GetValueOrDefault();
+
+        private static T RequiresEnumIsDefined<T>(T value, string paramName)
+            where T : struct
+        {
+            if (!Enum.IsDefined(typeof(T), value))
+            {
+                throw new ArgumentException($"Invalid {typeof(T).Name} value: {value}", paramName);
+            }
+
+            return value;
+        }
+
+        private static class Parser
+        {
+            private const string Prefix = "vssaas";
+            private const string ResourceTypes = "resourcetypes";
+            private const string ResourceTypeGroupName = "resourcetype";
+            private const string Instances = "instances";
+            private const string InstanceIdGroupName = "instanceid";
+            private const string Subscriptions = "subscriptions";
+            private const string SubscriptionIdGroupName = "subscriptionid";
+            private const string Locations = "locations";
+            private const string LocationGroupName = "location";
+            private const string AlphaNumericAndHyphen = "0-9a-zA-Z-";
+            private const RegexOptions Options = RegexOptions.CultureInvariant | RegexOptions.IgnoreCase | RegexOptions.Singleline;
+            private static readonly string IdFormat = $"{Prefix}/{ResourceTypes}/{{0}}/{Instances}/{{1}}/{Subscriptions}/{{2}}/{Locations}/{{3}}";
+            private static readonly string ResourceTypeGroup = $"(?<{ResourceTypeGroupName}>[{AlphaNumericAndHyphen}]+)";
+            private static readonly string InstanceIdGroup = $"(?<{InstanceIdGroupName}>[{AlphaNumericAndHyphen}]+)";
+            private static readonly string SubscriptionIdGroup = $"(?<{SubscriptionIdGroupName}>[{AlphaNumericAndHyphen}]+)";
+            private static readonly string LocationGroup = $"(?<{LocationGroupName}>[{AlphaNumericAndHyphen}]+)";
+            private static readonly string ResourceIdExpression = $"^{Prefix}/{ResourceTypes}/{ResourceTypeGroup}/{Instances}/{InstanceIdGroup}/{Subscriptions}/{SubscriptionIdGroup}/{Locations}/{LocationGroup}$";
+            private static readonly Regex ResourceIdRegEx = new Regex(ResourceIdExpression, Options);
+
+            public static string FormatId(ResourceType resourceType, Guid instanceId, Guid subscriptionId, AzureLocation location)
+            {
+                return string.Format(
+                    IdFormat,
+                    resourceType.ToString().ToLowerInvariant(),
+                    instanceId,
+                    subscriptionId,
+                    location.ToString().ToLowerInvariant());
+            }
+
+            public static bool TryParse(string id, out ResourceId value, out string reason)
+            {
+                reason = null;
+                value = default;
+
+                if (string.IsNullOrEmpty(id))
+                {
+                    value = Empty;
+                    return true;
+                }
+
+                var match = ResourceIdRegEx.Match(id);
+                if (!match.Success)
+                {
+                    reason = $"The id format is invalid: '{id}'. The expected format is {IdFormat}";
+                    return false;
+                }
+
+                // ResourceType
+                var resourceTypeValue = match.Groups[ResourceTypeGroupName].Value;
+                if (!Enum.TryParse<ResourceType>(resourceTypeValue, ignoreCase: true, out var resourceType))
+                {
+                    reason = $"Invalid {nameof(ResourceType)}: {resourceTypeValue}";
+                    return false;
+                }
+
+                // InstanceId
+                var instanceIdValue = match.Groups[InstanceIdGroupName].Value;
+                if (!Guid.TryParse(instanceIdValue, out var instanceId))
+                {
+                    reason = $"Invalid instance id {nameof(Guid)}: {instanceIdValue}";
+                    return false;
+                }
+
+                // SubscriptionId
+                var subscriptionIdValue = match.Groups[SubscriptionIdGroupName].Value;
+                if (!Guid.TryParse(subscriptionIdValue, out var subscriptionId))
+                {
+                    reason = $"Invalid subscription id {nameof(Guid)}: {subscriptionIdValue}";
+                    return false;
+                }
+
+                // Location
+                var locationValue = match.Groups[LocationGroupName].Value;
+                if (!Enum.TryParse<AzureLocation>(locationValue, ignoreCase: true, out var location))
+                {
+                    reason = $"Invalid {nameof(AzureLocation)}: {locationValue}";
+                    return false;
+                }
+
+                // Construct and return a valid ResourceId
+                value = new ResourceId(resourceType, instanceId, subscriptionId, location);
+                return true;
+            }
+        }
+    }
+}
---
+++ src/BackEnd/Common/Test/Common.Test/ResourceIdTests.cs
@@ -0,0 +1,272 @@
+// <copyright file="ResourceIdTests.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+using Microsoft.VsSaaS.Services.CloudEnvironments.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace Microsoft.VsSaaS.Services.CloudEnvironments.Common.Test
+{
+    public class ResourceIdTests
+    {
+        [Fact]
+        public void Empty()
+        {
+            // Empty from default field initializers
+            var empty = default(ResourceId);
+            Assert.Equal(Guid.Empty, empty.InstanceId);
+            Assert.Equal(Guid.Empty, empty.SubscriptionId);
+            Assert.Equal(default, empty.ResourceType);
+            Assert.Equal(default, empty.Location);
+            Assert.Null(empty.Id);
+
+            // Empty from static field
+            empty = ResourceId.Empty;
+            Assert.Equal(Guid.Empty, empty.InstanceId);
+            Assert.Equal(Guid.Empty, empty.SubscriptionId);
+            Assert.Equal(default, empty.ResourceType);
+            Assert.Equal(default, empty.Location);
+            Assert.Null(empty.Id);
+
+            // Empty from constructor
+            empty = new ResourceId(default, default, default, default);
+            Assert.Equal(Guid.Empty, empty.InstanceId);
+            Assert.Equal(Guid.Empty, empty.SubscriptionId);
+            Assert.Equal(default, empty.ResourceType);
+            Assert.Equal(default, empty.Location);
+            Assert.Null(empty.Id);
+
+            // Empty from Parse
+            empty = ResourceId.Parse(null);
+            Assert.Equal(Guid.Empty, empty.InstanceId);
+            Assert.Equal(Guid.Empty, empty.SubscriptionId);
+            Assert.Equal(default, empty.ResourceType);
+            Assert.Equal(default, empty.Location);
+            Assert.Null(empty.Id);
+
+            // Empty from Parse
+            empty = ResourceId.Parse(string.Empty);
+            Assert.Equal(Guid.Empty, empty.InstanceId);
+            Assert.Equal(Guid.Empty, empty.SubscriptionId);
+            Assert.Equal(default, empty.ResourceType);
+            Assert.Equal(default, empty.Location);
+            Assert.Null(empty.Id);
+        }
+
+        [Fact]
+        public void Ctor_OK()
+        {
+            var subscriptionId = Guid.NewGuid();
+            var instanceId = Guid.NewGuid();
+            var location = AzureLocation.AustraliaCentral;
+            var id = new ResourceId(ResourceType.ComputeVM, instanceId, subscriptionId, location);
+            Assert.Equal(ResourceType.ComputeVM, id.ResourceType);
+            Assert.Equal(instanceId, id.InstanceId);
+            Assert.Equal(subscriptionId, id.SubscriptionId);
+            Assert.Equal(location, id.Location);
+            Assert.Equal($"vssaas/resourcetypes/computevm/instances/{instanceId}/subscriptions/{subscriptionId}/locations/australiacentral", id.Id);
+        }
+
+        [Fact]
+        public void Ctor_throws()
+        {
+            var resourceType = ResourceType.ComputeVM;
+            var instanceId = Guid.NewGuid();
+            var subscriptionId = Guid.NewGuid();
+            var location = AzureLocation.AustraliaCentral;
+
+            Assert.Throws<ArgumentException>("resourceType", () => new ResourceId(default, instanceId, subscriptionId, location));
+            Assert.Throws<ArgumentException>("instanceId", () => new ResourceId(resourceType, default, subscriptionId, location));
+            Assert.Throws<ArgumentException>("subscriptionId", () => new ResourceId(resourceType, instanceId, default, location));
+            Assert.Throws<ArgumentException>("location", () => new ResourceId(resourceType, instanceId, subscriptionId, default));
+        }
+
+        [Fact]
+        public void Equality_Empty_Is_Empty()
+        {
+            var empty1 = ResourceId.Empty;
+            var empty2 = ResourceId.Empty;
+            Assert.True(Equals(empty1, empty2));
+            Assert.True(Equals(empty2, empty1));
+            Assert.Equal(empty1, empty2);
+            Assert.Equal(empty2, empty1);
+            Assert.True(empty1 == empty2);
+            Assert.True(empty2 == empty1);
+            Assert.True(empty1.Equals(empty2));
+            Assert.True(empty2.Equals(empty1));
+            Assert.Equal(empty1.GetHashCode(), empty2.GetHashCode());
+            Assert.Equal(0, empty1.GetHashCode());
+        }
+
+        [Fact]
+        public void Equality_Equals()
+        {
+            var id1 = NewTestResourceId();
+            var id2 = new ResourceId(id1.ResourceType, id1.InstanceId, id1.SubscriptionId, id1.Location);
+            Assert.True(Equals(id1, id2));
+            Assert.True(Equals(id2, id1));
+            Assert.Equal(id1, id2);
+            Assert.Equal(id2, id1);
+            Assert.True(id1 == id2);
+            Assert.True(id2 == id1);
+            Assert.True(id1.Equals(id2));
+            Assert.True(id2.Equals(id1));
+            Assert.Equal(id1.GetHashCode(), id2.GetHashCode());
+        }
+
+        [Fact]
+        public void Equality_Not_Equals()
+        {
+            var id1 = NewTestResourceId();
+            var id2 = NewTestResourceId();
+            Assert.False(Equals(id1, id2));
+            Assert.False(Equals(id2, id1));
+            Assert.NotEqual(id1, id2);
+            Assert.NotEqual(id2, id1);
+            Assert.True(id1 != id2);
+            Assert.True(id2 != id1);
+            Assert.False(id1.Equals(id2));
+            Assert.False(id2.Equals(id1));
+            Assert.NotEqual(id1.GetHashCode(), id2.GetHashCode());
+        }
+
+        [Fact]
+        public void Equality_Null_Not_Equals()
+        {
+            var id = NewTestResourceId();
+            Assert.False(Equals(id, null));
+            Assert.False(Equals(null, id));
+            Assert.True(id != null);
+            Assert.True(null != id);
+            Assert.False(id.Equals(null));
+        }
+
+        [Fact]
+        public void Equality_Not_Null()
+        {
+            var id = NewTestResourceId();
+            Assert.False(Equals(id, null));
+            Assert.False(Equals(null, id));
+            Assert.True(id != null);
+            Assert.True(null != id);
+            Assert.False(id.Equals(null));
+        }
+
+        [Fact]
+        public void ToString_and_ID_OK()
+        {
+            Assert.Null(ResourceId.Empty.ToString());
+            Assert.Null(ResourceId.Empty.Id);
+            var id = NewTestResourceId();
+            var idProp = id.Id;
+            var toString = id.ToString();
+            Assert.Equal(idProp, toString);
+            // Specific format
+            var expectedLocation = id.Location.ToString().ToLowerInvariant();
+            Assert.Equal($"vssaas/resourcetypes/computevm/instances/{id.InstanceId}/subscriptions/{id.SubscriptionId}/locations/{expectedLocation}", toString);
+        }
+
+        [Fact]
+        public void Parse_RoundTrip_OK()
+        {
+            var id1 = NewTestResourceId();
+            var id2 = ResourceId.Parse(id1.Id);
+            Assert.Equal(id1, id2);
+            Assert.True(ResourceId.TryParse(id1.Id, out id2));
+            Assert.Equal(id1, id2);
+        }
+
+        private const string InstanceId = "ebeea9c1-6898-4abb-b76f-ad087add2bda";
+        private const string SubscriptionId = "34da0f9b-78b3-4158-b1e9-0823f728fcf3";
+        public static TheoryData ParseFailsData =>
+            new TheoryData<int, string,bool>
+            {
+                // The first columun is a test id number for identifying test failures
+                { 0, null, true },
+                { 1, string.Empty, true },
+                { 2, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/eastus", true},
+                { 3, " ", false },
+                { 4, "garbage", false },
+                { 5, $" vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/eastus", false},
+                { 6, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/eastus ", false},
+                { 7, $"vs-saas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/eastus", false},
+                { 8, $"vssaas/resource-types/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/eastus", false},
+                { 9, $"vssaas/resourcetypes/compute-vm/instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/eastus", false},
+                { 10, $"vssaas/resourcetypes/computevm/Instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/eastus", true},
+                { 11, $"vssaas/resourcetypes/computevm/instances/{InstanceId}-/subscriptions/{SubscriptionId}/locations/eastus", false},
+                { 12, $"vssaas/resourcetypes/computevm/instances/{InstanceId}X/subscriptions/{SubscriptionId}/locations/eastus", false},
+                { 13, $"vssaas/resourcetypes/computevm/instances/{InstanceId}!/subscriptions/{SubscriptionId}/locations/eastus", false},
+                { 14, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/Subscriptions/{SubscriptionId}/locations/eastus", true},
+                { 15, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}-/locations/eastus", false},
+                { 16, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}X/locations/eastus", false},
+                { 17, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}!/locations/eastus", false},
+                { 18, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}/Locations/eastus", true},
+                { 19, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/east-us", false},
+                { 20, $"vssaas/resourcetypes/computevm/instances/{InstanceId}/subscriptions/{SubscriptionId}/locations/eastus!", false},
+            };
+
+        [Theory]
+        [MemberData(nameof(ParseFailsData))]
+        public void Parse_Theory(int num, string id, bool ok)
+        {
+            _ = num;
+
+            if (ok)
+            {
+                _ = ResourceId.Parse(id);
+                Assert.True(ResourceId.TryParse(id, out _));
+            }
+            else
+            {
+                Assert.Throws<FormatException>(() => _ = ResourceId.Parse(id));
+                Assert.False(ResourceId.TryParse(id, out _));
+            }
+        }
+
+        [Fact]
+        public void AzureLocation_UniqueValues()
+        {
+            // Ensure that no two AzureLocation entries have the same value
+            var values = Enum.GetValues(typeof(AzureLocation));
+            var valuesSet = new HashSet<int>();
+            foreach (var value in values)
+            {
+                var intValue = (int)value;
+                Assert.DoesNotContain(intValue, valuesSet);
+                valuesSet.Add(intValue);
+            }
+        }
+
+        [Fact]
+        public void ResourceId_ImplicitOperatorString()
+        {
+            var resourceId = NewTestResourceId();
+            string id = resourceId;
+            Assert.Equal(resourceId.Id, id);
+        }
+
+        [Fact]
+        public void ResourceId_ImplicitOperatorResourceType()
+        {
+            var resourceId = NewTestResourceId();
+            ResourceType type = resourceId;
+            Assert.Equal(resourceId.ResourceType, type);
+        }
+
+        [Fact]
+        public void ResourceId_ImplicitOperatorAzureLocation()
+        {
+            var resourceId = NewTestResourceId();
+            AzureLocation location = resourceId;
+            Assert.Equal(resourceId.Location, location);
+        }
+
+        private static ResourceId NewTestResourceId()
+        {
+            return new ResourceId(ResourceType.ComputeVM, Guid.NewGuid(), Guid.NewGuid(), AzureLocation.AustraliaCentral);
+        }
+    }
+}
