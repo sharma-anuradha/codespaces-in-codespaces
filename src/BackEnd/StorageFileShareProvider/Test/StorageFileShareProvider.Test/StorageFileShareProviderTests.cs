a1f3a1c32a19be27836337547dad387de806b3ed
Derek Bekoe <debekoe@microsoft.com>
2019-08-08 22:30:28 +0000

    Merged PR 195887: Storage File Share Provider Unit Tests
    Related work items: #949439

--- src/BackEnd/StorageFileShareProvider/Test/StorageFileShareProvider.Test/StorageFileShareProviderTests.cs
+++ src/BackEnd/StorageFileShareProvider/Test/StorageFileShareProvider.Test/StorageFileShareProviderTests.cs
@@ -3,22 +3,167 @@
 // </copyright>
 
 using System;
-using System.Reflection;
 using System.Threading.Tasks;
-using Microsoft.VsSaaS.Services.CloudEnvironments.StorageFileShareProvider;
 using Microsoft.VsSaaS.Services.CloudEnvironments.StorageFileShareProvider.Models;
 using Microsoft.VsSaaS.Services.CloudEnvironments.StorageFileShareProvider.Abstractions;
-// using Moq;
+using Moq;
 using Xunit;
 
 namespace Microsoft.VsSaaS.Services.CloudEnvironments.StorageFileShareProvider.Test
 {
     public class StorageFileShareProviderTests
     {
-        // TODO Mock IStorageFileShareProviderHelper and add tests.
+        private const string MockResourceId = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/TestRg1/providers/Microsoft.Storage/storageAccounts/TestStorageAccount";
+        private const string MockResourceGroup = "TestRg1";
+        private const string MockLocation = "westus2";
+        private const string MockSubscriptionId = "00000000-0000-0000-0000-000000000000";
+        private const string MockStorageBlobUrl = "https://staccname.blob.core.windows.net/containername/blobname";
+        private const string MockStorageAccountName = "vsoce123";
+        private const string MockStorageAccountKey = "SecretKey";
+        private const string MockStorageShareName = "cloudenvdata";
+        private const string MockStorageFileName = "dockerlib";
+
         [Fact]
-        public async Task FileShare_Ok()
+        public void Ctor_with_bad_options()
         {
+            Assert.Throws<ArgumentNullException>(() => new StorageFileShareProvider(null));
+        }
+
+        /// <summary>
+        /// Create operation succeeds.
+        /// </summary>
+        [Fact]
+        public async Task FileShare_Create_Ok()
+        {
+            var providerHelperMoq = new Mock<IStorageFileShareProviderHelper>();
+            var mockCheckPrepareResults = new[] { 0.0, 0.5, 0.7, 1 };
+            providerHelperMoq
+                .Setup(x => x.CreateStorageAccountAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(MockResourceId);
+            providerHelperMoq
+                .Setup(x => x.CreateFileShareAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+            providerHelperMoq
+                .Setup(x => x.StartPrepareFileShareAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+            
+            providerHelperMoq
+                .SetupSequence(x => x.CheckPrepareFileShareAsync(It.IsAny<string>()))
+                .ReturnsAsync(mockCheckPrepareResults[0])
+                .ReturnsAsync(mockCheckPrepareResults[1])
+                .ReturnsAsync(mockCheckPrepareResults[2])
+                .ReturnsAsync(mockCheckPrepareResults[3]);
+
+            var storageProvider = new StorageFileShareProvider(providerHelperMoq.Object);
+
+            var input = new FileShareProviderCreateInput()
+            {
+                AzureResourceGroup = MockResourceGroup,
+                AzureLocation = MockLocation,
+                AzureSubscription = MockSubscriptionId,
+                StorageBlobUrl = MockStorageBlobUrl,
+            };
+
+            string continuationToken = null;
+
+            // 3 because there are 3 steps before we wait for the preparation to complete.
+            // 1. Create Storage Account
+            // 2. Create File Share
+            // 3. Start Prepare File Share
+            // Wait for preparation to complete...
+            int expectedIterations = 3 + mockCheckPrepareResults.Length;
+
+            for (int iteration = 0; iteration < expectedIterations; iteration++)
+            {
+                if (iteration == 0)
+                {
+                    // At the beginning, continuation token should be null.
+                    Assert.Null(continuationToken);
+                }
+                var result = await storageProvider.CreateAsync(input, continuationToken);
+                // result should not be null after each iteration
+                Assert.NotNull(result);
+                // ResourceId should not be null after each iteration
+                Assert.NotNull(result.ResourceId);
+                continuationToken = result.ContinuationToken;
+            }
+            // After all the steps, continuation token should be null as that's how we signal completion.
+            Assert.Null(continuationToken);
+        }
+
+        /// <summary>
+        /// Delete operation succeeds.
+        /// We check that there is no continuation token at the end as Delete is a one step operation.
+        /// </summary>
+        [Fact]
+        public async Task FileShare_Delete_Ok()
+        {
+            var providerHelperMoq = new Mock<IStorageFileShareProviderHelper>();
+            providerHelperMoq
+                .Setup(x => x.DeleteStorageAccountAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+            var storageProvider = new StorageFileShareProvider(providerHelperMoq.Object);
+            var input = new FileShareProviderDeleteInput()
+            {
+                ResourceId = MockResourceId,
+            };
+            var result = await storageProvider.DeleteAsync(input);
+            Assert.NotNull(result);
+            Assert.Null(result.ContinuationToken);
+        }
+
+        /// <summary>
+        /// Delete operation raise an exception on null input.
+        /// </summary>
+        [Fact]
+        public async Task FileShare_Delete_Null_Input()
+        {
+            var providerHelperMoq = new Mock<IStorageFileShareProviderHelper>();
+            var storageProvider = new StorageFileShareProvider(providerHelperMoq.Object);
+            FileShareProviderDeleteInput input = null;
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await storageProvider.DeleteAsync(input));
+        }
+
+        /// <summary>
+        /// Assign operation succeeds and returns the correct connection info.
+        /// We check that there is no continuation token at the end as Delete is a one step operation.
+        /// </summary>
+        [Fact]
+        public async Task FileShare_Assign_Ok()
+        {
+            var providerHelperMoq = new Mock<IStorageFileShareProviderHelper>();
+            var mockConnInfo = new ShareConnectionInfo(
+                MockStorageAccountName,
+                MockStorageAccountKey,
+                MockStorageShareName,
+                MockStorageFileName);
+            providerHelperMoq
+                .Setup(x => x.GetConnectionInfoAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(mockConnInfo));
+            var storageProvider = new StorageFileShareProvider(providerHelperMoq.Object);
+            var input = new FileShareProviderAssignInput()
+            {
+                ResourceId = MockResourceId,
+            };
+            var result = await storageProvider.AssignAsync(input);
+            Assert.NotNull(result);
+            Assert.Equal(MockStorageAccountName, result.StorageAccountName);
+            Assert.Equal(MockStorageAccountKey, result.StorageAccountKey);
+            Assert.Equal(MockStorageShareName, result.StorageShareName);
+            Assert.Equal(MockStorageFileName, result.StorageFileName);
+            Assert.Null(result.ContinuationToken);
+        }
+
+        /// <summary>
+        /// Assign operation raise an exception on null input.
+        /// </summary>
+        [Fact]
+        public async Task FileShare_Assign_Null_Input()
+        {
+            var providerHelperMoq = new Mock<IStorageFileShareProviderHelper>();
+            var storageProvider = new StorageFileShareProvider(providerHelperMoq.Object);
+            FileShareProviderAssignInput input = null;
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await storageProvider.AssignAsync(input));
         }
     }
 }
