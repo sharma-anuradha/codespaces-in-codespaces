bea69414becceee5fe750936a18da2e179e088f1
John Keech <jokeech@microsoft.com>
2019-09-11 07:56:05 +0000

    Merged PR 201845: Update resource wipe script to perform batch deletions in parallel
    Update resource wipe script to perform batch deletions in parallel

--- tools/cmd/wipe-resources.ps1
+++ tools/cmd/wipe-resources.ps1
@@ -1,5 +1,26 @@
 param([string] $subscription, [string] $stamp)
 
+$BatchSize = 50
+$script:backgroundJobs = @()
+function Wait-Jobs() {
+    if ($script:backgroundJobs.Count -gt 0) {
+        Write-Host "Waiting for all background deletion jobs to complete..."
+        $jobs = Wait-Job -Job $script:backgroundJobs
+        Receive-Job $jobs
+        Write-Host "Done"
+        $script:backgroundJobs = @()
+    }
+}
+
+function Start-BackgroundJob($Block, $Arguments, $Name) {
+    $script:backgroundJobs += Start-Job -ScriptBlock $Block -ArgumentList $Arguments -Name $Name
+
+    # Don't spin up too many PS background jobs at once or you will run out of memory
+    if ($script:backgroundJobs.Count -ge $BatchSize) {
+        Wait-Jobs
+    }
+}
+
 Write-Host "Deleting $stamp data plane resources from subscription $subscription"
 $resourcegroups = az group list --subscription $subscription | ConvertFrom-Json
 $toDelete = $resourcegroups | Where-Object { $_.name -like "$stamp-*" }
@@ -7,6 +28,9 @@ $total = $toDelete.Count
 $count = 1
 $toDelete | ForEach-Object {
     Write-Host "[$count/$total] Deleting $($_.name)"
-    az group delete --name $_.name --subscription $subscription -y --no-wait
     $count++
+    $block = {
+        az group delete --name $args[0] --subscription $args[1] -y --no-wait
+    }
+    Start-BackgroundJob -Block $block -Arguments @($_.name, $subscription) -Name "DeleteRG_$($_.name)"
 }
