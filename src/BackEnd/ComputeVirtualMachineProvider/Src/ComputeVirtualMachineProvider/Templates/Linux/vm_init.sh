abc0ea17095979e37cb10b937dc1dd13ee116496
Anuradha Sharma <anush@microsoft.com>
2019-09-19 18:08:55 +0000

    Merged PR 203303: check return code for vm initialization.
    check return code for vm initialization.

--- src/BackEnd/ComputeVirtualMachineProvider/Src/ComputeVirtualMachineProvider/Templates/Linux/vm_init.sh
+++ src/BackEnd/ComputeVirtualMachineProvider/Src/ComputeVirtualMachineProvider/Templates/Linux/vm_init.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
+echo "Set script to fail if command in script returns non zero return code"
+set -eu pipefall
+
 SCRIPT_PARAM_VMTOKEN=''
 echo $SCRIPT_PARAM_VMTOKEN > /.vmtoken
+
 echo "Updating packages ..."
-apt update
-apt upgrade -y
+apt update || true && apt upgrade -y 
+
 echo "Increase file watcher limit"
 echo "fs.inotify.max_user_watches=524288" | tee -a /etc/sysctl.conf
 sysctl -p
+
 echo "Install docker ..."
 apt-get install -y docker.io
-echo "Install ORAS ..."
-curl -LO https://github.com/deislabs/oras/releases/download/v0.5.0/oras_0.5.0_linux_amd64.tar.gz
-# unpack, install, dispose
-mkdir -p oras-install/
-tar -zxf oras_0.5.0_*.tar.gz -C oras-install/
-mv oras-install/oras /usr/local/bin/
-rm -rf oras_0.5.0_*.tar.gz oras-install/
-echo "Pull VM Agent..."
-mkdir /vmagent
-cd /vmagent
-#oras pull -u <userid> -p <password> <acrUri/image:tag>
-#apt-get install unzip
-#unzip CLI_linux*.zip
