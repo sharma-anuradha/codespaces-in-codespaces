112f1c4b76f1cd3e3ec423ffc6ef8f7a8da688f7
Peter Lisy <pelisy@microsoft.com>
2019-10-07 22:12:17 +0000

    Merged PR 206521: chore: Removed loading devblogs assets when not navigating to BlogPost.
    chore: Removed loading devblogs assets when not navigating to BlogPost.

--- src/services/containers/VsClk.Portal.WebSite/ClientApp/src/BlogPost.tsx
+++ src/services/containers/VsClk.Portal.WebSite/ClientApp/src/BlogPost.tsx
@@ -1,13 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { ApplicationState } from './reducers/rootReducer';
+
 export function BlogPost() {
     if (process.env.NODE_ENV === 'development') {
         return <Redirect to='/welcome' />;
     }
+
+    const isAuthenticated = useSelector(
+        (state: ApplicationState) => state.authentication.isAuthenticated
+    );
+
     useEffect(() => {
+        if (isAuthenticated) {
+            return;
+        }
+
         window.location.replace(
             'https://devblogs.microsoft.com/visualstudio/intelligent-productivity-and-collaboration-from-anywhere/'
         );
     });
-    return <></>;
+
+    return <Fragment />;
 }
