a741631b85c900be23f49eeef92b82aa837fc524
Rick Krause <rickkr@microsoft.com>
2019-10-17 22:01:22 +0000

    Merged PR 208440: add script to copy nexus images to control plane sub
    Attempts to copy images to control plane subs are sometimes timing out. The az extension we've been using uses background copy logic which has random perf--sometimes OK and sometimes very poor.
    
    To get around this I created a script that is based on the [sources for that tool](https://github.com/Azure/azure-cli-extensions/tree/master/src/image-copy) (which simply runs a set of az commands). I replaced the _az storage blob copy start_ command (which does the background copy) with a download/upload pair. Thanks @<Derek Bekoe>  for the help!
    
    I have tested the commands locally (interactively as I built up the script). I'm testing the script now with [this release](https://dev.azure.com/devdiv/OnlineServices/_releaseProgress?_a=release-environment-logs&releaseId=2772&environmentId=12756), which is using my topic branch for the source repo. (I made a [backup copy of the release pipe](https://dev.azure.com/devdiv/OnlineServices/_releaseDefinition?definitionId=81&_a=environments-editor-preview) prior to changing it in case I need to throw this work away.)
    
    Related work items: #1002684, #989747

---
+++ .pipelines/CopyWindowsImage.ps1
@@ -0,0 +1,64 @@
+param(
+    [Parameter(Mandatory)]
+    [string]$ImageName,
+    [Parameter(Mandatory)]
+    [string]$TargetSubscription,
+    [Parameter(Mandatory)]
+    [string]$TargetGroupName,
+    [Parameter(Mandatory)]
+    [string]$WorkPath
+)
+$ProgressPreference = 'SilentlyContinue'
+$SourceSubscription = 'cd02057e-7b97-4159-b924-e8392142ee1e'
+$TargetLocation = 'westus2'
+
+az account set -s $SourceSubscription
+
+$SourceImage = (az image list | ConvertFrom-Json) | Where-Object name -eq $ImageName
+if ($null -eq $SourceImage) {
+    throw "Cannot find $ImageName image in $SourceSubscription subscription"
+}
+if ($SourceImage -is [object[]]) {
+    throw "Found more than one image named $ImageName in $SourceSubscription subscription"
+}
+
+Write-Host "Snapshot $ImageName"
+$SnapshotName = "$($ImageName)_os_disk_snapshot"
+az snapshot create -n $SnapshotName -g $SourceImage.resourceGroup --source $SourceImage.storageProfile.osDisk.managedDisk.id
+
+Write-Host "Download $SnapshotName"
+$BlobPath = Join-Path $WorkPath "$SnapshotName.vhd"
+$Access = az snapshot grant-access --duration-in-seconds 36000 -n $SnapshotName -g $SourceImage.resourceGroup | ConvertFrom-Json
+$Start = Get-Date
+Invoke-WebRequest $Access.accessSas -OutFile $BlobPath
+$Duration = (Get-Date) - $Start
+Write-Host "Download completed in $($Duration.TotalMinutes) minutes"
+az snapshot revoke-access -n $SnapshotName -g $SourceImage.resourceGroup
+az snapshot delete -n $SnapshotName -g $SourceImage.resourceGroup
+
+$TempGroupName = "image-copy-$ImageName"
+Write-Host "Create $TempGroupName Resource Group"
+az group create -n $TempGroupName -l $TargetLocation --subscription $TargetSubscription
+
+$TempStorageName = "$TargetLocation$ImageName".ToLower()
+Write-Host "Create $TempStorageName Storage Account"
+$Storage = az storage account create -n $TempStorageName -g $TempGroupName -l $TargetLocation --subscription $TargetSubscription --kind StorageV2 --sku Premium_LRS | ConvertFrom-Json
+
+$TempContainerName = 'snapshots'
+Write-Host "Create $TempContainerName Storage Container"
+az storage container create -n $TempContainerName --account-name $TempStorageName --subscription $TargetSubscription
+
+Write-Host "Upload $BlobPath"
+$Start = Get-Date
+az storage blob upload -c $TempContainerName -f $BlobPath -n $ImageName --account-name $TempStorageName --subscription $TargetSubscription --no-progress
+$Duration = (Get-Date) - $Start
+Write-Host "Upload completed in $($Duration.TotalMinutes) minutes"
+
+Write-Host "Snapshot $ImageName"
+$Snapshot = az snapshot create -n $SnapshotName -g $TempGroupName -l $TargetLocation --source "$($Storage.primaryEndpoints.blob)$TempContainerName/$ImageName" --subscription $TargetSubscription | ConvertFrom-Json
+
+Write-Host "Create $ImageName Image"
+az image create -n $ImageName -g $TargetGroupName -l $TargetLocation --os-type $SourceImage.storageProfile.osDisk.osType --source $Snapshot.id --subscription $TargetSubscription
+
+Write-Host "Remove $TempGroupName Resource Group"
+az group delete -n $TempGroupName --subscription $TargetSubscription --no-wait --yes
