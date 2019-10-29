3f83e67f793685e408b53964446e78d0f6f41556
Derek Bekoe <debekoe@microsoft.com>
2019-10-29 16:32:01 +0000

    Merged PR 210845: Fix iptables blocking of Instance Metadata Service not working
    Fix iptables blocking of Instance Metadata Service
    
    Previously, the change wasn't taking effect due to piping with `| debconf-set-selections` in combination with the &&.
    It worked in my local testing before as I did all my tests with access to a tty.
    
    Now, we don't use && anymore and just run it in sequence.
    Verified in my own personal stamp.
    
    Also, I tried the NSG approach and that still didn't work. My guess is the packets don't actually reach the NSG. I think it's due to this:
    "The endpoint is available at a well-known **non-routable IP address** (169.254.169.254) that can be accessed **only from within the VM**." from https://docs.microsoft.com/en-us/azure/virtual-machines/windows/instance-metadata-service.

--- src/BackEnd/ComputeVirtualMachineProvider/Src/ComputeVirtualMachineProvider/Templates/Linux/vm_init.sh
+++ src/BackEnd/ComputeVirtualMachineProvider/Src/ComputeVirtualMachineProvider/Templates/Linux/vm_init.sh
@@ -27,13 +27,13 @@ apt-get install -y docker.io
 # Block Azure Instance Metadata Service IP on host (OUTPUT) and also in containers (DOCKER-USER)
 # This needs to happen after the docker install for DOCKER-USER to exist in iptables.
 echo "Block Azure Instance Metadata Service ..."
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections \
-  && echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections \
-  && apt-get install -y iptables-persistent \
-  && INSTANCE_METADATA_IP=169.254.169.254 \
-  && iptables -I OUTPUT -d $INSTANCE_METADATA_IP -j DROP \
-  && iptables -I DOCKER-USER -d $INSTANCE_METADATA_IP -j DROP \
-  && iptables-save > /etc/iptables/rules.v4
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+apt-get install -y iptables-persistent
+INSTANCE_METADATA_IP=169.254.169.254
+iptables -I OUTPUT -d $INSTANCE_METADATA_IP -j DROP
+iptables -I DOCKER-USER -d $INSTANCE_METADATA_IP -j DROP
+iptables-save > /etc/iptables/rules.v4
 
 echo "Install unzip ..."
 apt-get -yq update && apt-get install -y --no-install-recommends unzip
