27398353286c891fd4c2614dcd5b229c8616c0e2
Eljo George <elpadann@microsoft.com>
2019-10-29 08:23:01 +0000

    Merged PR 210235: Remove the old code for share folder creation
    - Remove the old code for share folder creation
    
    Related work items: #985854

--- src/BackEnd/ComputeVirtualMachineProvider/Src/ComputeVirtualMachineProvider/Templates/Linux/vm_init.sh
+++ src/BackEnd/ComputeVirtualMachineProvider/Src/ComputeVirtualMachineProvider/Templates/Linux/vm_init.sh
@@ -43,15 +43,6 @@ mkdir -p /.vsonline/vsoagent/bin
 cd /.vsonline/vsoagent/bin
 wget -qO- -O tmp.zip $SCRIPT_PARAM_VMAGENT_BLOB_URL && unzip tmp.zip && rm tmp.zip
 
-# To be removed in favor of line #57
-echo "Create vso shared folder ..."
-mkdir -p /.vsonline/.vsoshared
-
-# To be removed in favor of line #58 and 59
-echo "Create monitor files"
-touch /.vsonline/.vsoshared/connection-monitor.json
-chmod o+rw /.vsonline/.vsoshared/connection-monitor.json
-
 echo "Create vso shared folder with appropriate permissions"
 vsoSharedFolder=~/.vsonline/.vsoshared
 mkdir -p $vsoSharedFolder
