e9b71850968ca67d37f00d477b6b73c0f582f33c
Trevor Short <trevors@microsoft.com>
2019-11-01 20:02:01 +0000

    Merged PR 210994: First set of changes to stop downloading the image to disk and to just copy
    First set of changes to stop downloading the image to disk and to just copy straight from one subscription to another.

--- .pipelines/CopyWindowsImage.ps1
+++ .pipelines/CopyWindowsImage.ps1
@@ -22,6 +22,8 @@ if ($SourceImage -is [object[]]) {
     throw "Found more than one image named $ImageName in $SourceSubscription subscription"
 }
 
+$TargetSubName = $TargetSubscription.Substring("vsclk-core-".Length)
+
 # Download and extract the latest v10 version of azCopy to our work folder
 $AzCopyZipPath = Join-Path $WorkPath "azCopy.zip"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
@@ -30,43 +32,41 @@ Expand-Archive -Path $AzCopyZipPath -DestinationPath $WorkPath -Force
 $AzCopyExe = Get-ChildItem $WorkPath/*/azcopy.exe
 
 Write-Host "Snapshot $ImageName"
-$SnapshotName = "$($ImageName)_os_disk_snapshot"
+$SnapshotName = "$($ImageName)_snapshot_$TargetSubName"
 az snapshot create -n $SnapshotName -g $SourceImage.resourceGroup --source $SourceImage.storageProfile.osDisk.managedDisk.id
 
-Write-Host "Download $SnapshotName"
-$BlobPath = Join-Path $WorkPath "$SnapshotName.vhd"
-$Access = az snapshot grant-access --duration-in-seconds 36000 -n $SnapshotName -g $SourceImage.resourceGroup | ConvertFrom-Json
-$Start = Get-Date
-& $AzCopyExe cp $Access.accessSas $BlobPath --check-md5 LogOnly
-$Duration = (Get-Date) - $Start
-Write-Host "Download completed in $($Duration.TotalMinutes) minutes"
-az snapshot revoke-access -n $SnapshotName -g $SourceImage.resourceGroup
-az snapshot delete -n $SnapshotName -g $SourceImage.resourceGroup
-
-$TempGroupName = "image-copy-$ImageName"
+$TempGroupName = "image-copy-$ImageName-$TargetSubName"
 Write-Host "Create $TempGroupName Resource Group"
 az group create -n $TempGroupName -l $TargetLocation --subscription $TargetSubscription
 
-$TempStorageName = "$TargetLocation$ImageName".ToLower()
+$TempStorageName = "$TargetLocation$TargetSubName$ImageName".ToLower()
 Write-Host "Create $TempStorageName Storage Account"
 $Storage = az storage account create -n $TempStorageName -g $TempGroupName -l $TargetLocation --subscription $TargetSubscription --kind StorageV2 --sku Premium_LRS | ConvertFrom-Json
 
-$TempContainerName = 'snapshots'
+$TempContainerName = "snapshots$TargetSubName"
 Write-Host "Create $TempContainerName Storage Container"
 az storage container create -n $TempContainerName --account-name $TempStorageName --subscription $TargetSubscription
 
-Write-Host "Upload $BlobPath"
+Write-Host "Obtaining the appropriate SAS Url's needed for copying."
+$SourceAccess = az snapshot grant-access --duration-in-seconds 36000 -n $SnapshotName -g $SourceImage.resourceGroup | ConvertFrom-Json
+$ExpiryDate = (Get-Date).AddSeconds(36000).ToUniversalTime().ToString('s') + "Z"
+$TargetAccess = az storage blob generate-sas --container-name $TempContainerName --account-name $TempStorageName --name $ImageName --permissions acdrw --full-uri --expiry $ExpiryDate --subscription $TargetSubscription
+
+Write-Host "Copy $SnapshotName from $SourceSubscription to $TargetSubscription"
 $Start = Get-Date
-az storage blob upload -c $TempContainerName -f $BlobPath -n $ImageName --account-name $TempStorageName --subscription $TargetSubscription --no-progress
+& $AzCopyExe copy $SourceAccess.accessSas $TargetAccess
 $Duration = (Get-Date) - $Start
-Write-Host "Upload completed in $($Duration.TotalMinutes) minutes"
-Remove-Item $BlobPath -Force -ErrorAction Continue
+Write-Host "Copy completed in $($Duration.TotalMinutes) minutes"
 
 Write-Host "Snapshot $ImageName"
 $Snapshot = az snapshot create -n $SnapshotName -g $TempGroupName -l $TargetLocation --source "$($Storage.primaryEndpoints.blob)$TempContainerName/$ImageName" --subscription $TargetSubscription | ConvertFrom-Json
 
 Write-Host "Create $ImageName Image"
 az image create -n $ImageName -g $TargetGroupName -l $TargetLocation --os-type $SourceImage.storageProfile.osDisk.osType --source $Snapshot.id --subscription $TargetSubscription
+
+Write-Host "Remove $SnapshotName"
+az snapshot revoke-access -n $SnapshotName -g $SourceImage.resourceGroup
+az snapshot delete -n $SnapshotName -g $SourceImage.resourceGroup
 
 Write-Host "Remove $TempGroupName Resource Group"
 az group delete -n $TempGroupName --subscription $TargetSubscription --no-wait --yes
