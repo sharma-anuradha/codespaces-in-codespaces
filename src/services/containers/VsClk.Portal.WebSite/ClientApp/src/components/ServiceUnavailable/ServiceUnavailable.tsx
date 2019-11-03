481a786cea8c30b57603ebf5a537b27ad0b24eb6
Peter Lisy <pelisy@microsoft.com>
2019-11-03 01:00:24 +0000

    Merged PR 212013: feat: Added form to subscribe for notifications.
    feat: Added form to subscribe for notifications.
    
    Strings need review @<Nik Molnar> / @<Jonathan Carter>  but we can change those in another PR.

--- src/services/containers/VsClk.Portal.WebSite/ClientApp/src/components/ServiceUnavailable/ServiceUnavailable.tsx
+++ src/services/containers/VsClk.Portal.WebSite/ClientApp/src/components/ServiceUnavailable/ServiceUnavailable.tsx
@@ -1,11 +1,114 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState, FormEventHandler } from 'react';
+import { useSelector } from 'react-redux';
 
-import { Stack, StackItem, Text } from 'office-ui-fabric-react';
+import {
+    Stack,
+    StackItem,
+    Text,
+    TextField,
+    PrimaryButton,
+    ITextFieldProps,
+    Label,
+    MessageBar,
+    MessageBarType,
+} from 'office-ui-fabric-react';
+
 import { PortalLayout } from '../portalLayout/portalLayout';
 import { EverywhereImage } from '../EverywhereImage/EverywhereImage';
 import { unavailableErrorMessage } from '../../actions/serviceUnavailable';
+import { notificationsSubscribe } from '../../actions/notificationsSubscribe';
+import { ApplicationState } from '../../reducers/rootReducer';
+import { useDispatch } from '../../actions/middleware/useDispatch';
 
 import './ServiceUnavailable.css';
+
+const NoUserInfoForm: FC = () => null;
+const FormWithEmail: FC<{ defaultUserEmail: string }> = ({ defaultUserEmail }) => {
+    const [userEmail, setEmail] = useState(defaultUserEmail);
+    const [disabled, setDisabled] = useState(false);
+    const dispatch = useDispatch();
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+    const postEmail: FormEventHandler = useCallback(
+        async (event) => {
+            event.preventDefault();
+
+            setDisabled(true);
+
+            try {
+                await dispatch(notificationsSubscribe(userEmail));
+                setShowSuccessMessage(true);
+            } catch {
+                setShowErrorMessage(true);
+                setDisabled(false);
+            }
+        },
+        [userEmail]
+    );
+
+    const emailChangedCallback: ITextFieldProps['onChange'] = useCallback((_event, value = '') => {
+        setEmail(value);
+    }, []);
+
+    return (
+        <form onSubmit={postEmail}>
+            <Stack horizontalAlign='center'>
+                <Stack.Item>
+                    <Label id='email-label' htmlFor='email'>
+                        We can send you an email when we are ready to enable environment creation
+                        again.
+                    </Label>
+                </Stack.Item>
+                <Stack.Item>
+                    <Stack horizontal>
+                        <TextField
+                            id='email'
+                            type='email'
+                            value={userEmail}
+                            onChange={emailChangedCallback}
+                            aria-labelledby='email-label'
+                            className='email-form__text-field'
+                        />
+                        <PrimaryButton
+                            type='submit'
+                            className='email-form__button'
+                            disabled={disabled}
+                        >
+                            Notify Me
+                        </PrimaryButton>
+                    </Stack>
+                </Stack.Item>
+                <Stack.Item>
+                    {showSuccessMessage && (
+                        <MessageBar messageBarType={MessageBarType.success} style={{ width: 358 }}>
+                            We'll let you know.
+                        </MessageBar>
+                    )}
+                </Stack.Item>
+                <Stack.Item>
+                    {showErrorMessage && (
+                        <MessageBar messageBarType={MessageBarType.error} style={{ width: 358 }}>
+                            Something went wrong. Please try again later.
+                        </MessageBar>
+                    )}
+                </Stack.Item>
+            </Stack>
+        </form>
+    );
+};
+
+const NotificationsForm: FC = () => {
+    const { defaultUserEmail } = useSelector((state: ApplicationState) => ({
+        defaultUserEmail: (state.userInfo && state.userInfo.mail) || '',
+    }));
+
+    if (!defaultUserEmail) {
+        return <NoUserInfoForm />;
+    }
+
+    return <FormWithEmail defaultUserEmail={defaultUserEmail} />;
+};
 
 export const ServiceUnavailable: FC = () => {
     return (
@@ -27,6 +130,9 @@ export const ServiceUnavailable: FC = () => {
 
                 <Stack.Item className='service-unavailable-page__message'>
                     {unavailableErrorMessage}
+                </Stack.Item>
+                <Stack.Item>
+                    <NotificationsForm />
                 </Stack.Item>
             </Stack>
         </PortalLayout>
