a88c5c27714fe25ebb54949d6eec741f5c1fe1f0
Rick Krause <rickkr@microsoft.com>
2019-11-05 18:16:00 +0000

    Merged PR 211885: add v2 image script to support sig per region
    Adding a v2 image script to support having a Shared Image Gallery (SIG) per region. We expect this will allow us to deploy images more quickly. (Creating a separate script so we can continue to work on improving the v1 pipeline while we're bringing up v2. Once v2 is fully onboarded I'll delete the old script and rename this one to the old name.)
    
    If you compare this with the [original script](https://dev.azure.com/devdiv/OnlineServices/_git/vsclk-core?path=%2F.pipelines%2FCopyWindowsImage.ps1&_a=content) you may notice I also removed the use of a temp RG for the image copy. Instead I'm creating the temp Storage Account in the RG containing the SIG.
    
    I also don't decorate the temp SA and container names with target sub name and location because those are already handled by virtue of them being in the target sub and in an RG that is specific to the location.
    
    Related work items: #989763

---
+++ .pipelines/CopyWindowsImage2.ps1
@@ -0,0 +1,68 @@
+param(
+    [Parameter(Mandatory)]
+    [string]$ImageName,
+    [Parameter(Mandatory)]
+    [string]$TargetSubscription,
+    [Parameter(Mandatory)]
+    [string]$TargetGroupName,
+    [Parameter(Mandatory)]
+    [string]$TargetLocation,
+    [Parameter(Mandatory)]
+    [string]$WorkPath
+)
+$ProgressPreference = 'SilentlyContinue'
+$SourceSubscription = 'cd02057e-7b97-4159-b924-e8392142ee1e'
+
+az account set -s $SourceSubscription
+
+$SourceImage = (az image list | ConvertFrom-Json) | Where-Object name -eq $ImageName
+if ($null -eq $SourceImage) {
+    throw "Cannot find $ImageName image in $SourceSubscription subscription"
+}
+if ($SourceImage -is [object[]]) {
+    throw "Found more than one image named $ImageName in $SourceSubscription subscription"
+}
+
+# Download and extract the latest v10 version of azCopy to our work folder
+$AzCopyZipPath = Join-Path $WorkPath "azCopy.zip"
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile $AzCopyZipPath -UseBasicParsing
+Expand-Archive -Path $AzCopyZipPath -DestinationPath $WorkPath -Force
+$AzCopyExe = Get-ChildItem $WorkPath/*/azcopy.exe
+
+Write-Host "Snapshot $ImageName"
+$SnapshotName = "$($ImageName)_snapshot_$($TargetSubscription -replace '-', '_')"
+az snapshot create -n $SnapshotName -g $SourceImage.resourceGroup --source $SourceImage.storageProfile.osDisk.managedDisk.id
+
+$TempStorageName = $ImageName.ToLower()
+Write-Host "Create $TempStorageName Storage Account"
+$Storage = az storage account create -n $TempStorageName -g $TargetGroupName -l $TargetLocation --subscription $TargetSubscription --sku Premium_LRS --https-only true | ConvertFrom-Json
+
+$TempContainerName = "snapshot"
+Write-Host "Create $TempContainerName Storage Container"
+az storage container create -n $TempContainerName --account-name $TempStorageName --subscription $TargetSubscription
+
+Write-Host "Grant AzCopy Access to Copy $SnapshotName"
+$SourceAccess = az snapshot grant-access --duration-in-seconds 36000 -n $SnapshotName -g $SourceImage.resourceGroup | ConvertFrom-Json
+$ExpiryDate = (Get-Date).AddSeconds(36000).ToUniversalTime().ToString('s') + "Z"
+$TargetAccess = az storage blob generate-sas --container-name $TempContainerName --account-name $TempStorageName --name $ImageName --permissions acdrw --full-uri --expiry $ExpiryDate --subscription $TargetSubscription
+
+Write-Host "Copy $SnapshotName from $SourceSubscription to $TargetSubscription"
+$Start = Get-Date
+& $AzCopyExe copy $SourceAccess.accessSas $TargetAccess
+$Duration = (Get-Date) - $Start
+Write-Host "Copy completed in $($Duration.TotalMinutes) minutes"
+
+Write-Host "Snapshot $ImageName"
+$Snapshot = az snapshot create -n $SnapshotName -g $TargetGroupName -l $TargetLocation --source "$($Storage.primaryEndpoints.blob)$TempContainerName/$ImageName" --subscription $TargetSubscription | ConvertFrom-Json
+
+Write-Host "Create $ImageName Image"
+az image create -n $ImageName -g $TargetGroupName -l $TargetLocation --os-type $SourceImage.storageProfile.osDisk.osType --source $Snapshot.id --subscription $TargetSubscription
+
+Write-Host "Remove $SnapshotName"
+az snapshot revoke-access -n $SnapshotName -g $SourceImage.resourceGroup
+az snapshot delete -n $SnapshotName -g $SourceImage.resourceGroup
+az snapshot delete -n $SnapshotName -g $TargetGroupName --subscription $TargetSubscription
+
+Write-Host "Remove $TempStorageName Storage Account"
+az storage account delete -n $TempStorageName -g $TargetGroupName --subscription $TargetSubscription --yes
