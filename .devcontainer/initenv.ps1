b4b0598c9335706bb84cf0866c5f199e3566ecff
Rajkumar Janakiraman <janraj@microsoft.com>
2020-02-19 23:10:42 +0000

    Merged PR 230154: nexus dogfood init
    nexus dogfood init

---
+++ .devcontainer/initenv.ps1
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/microsoft/artifacts-credprovider/master/helpers/installcredprovider.ps1 -o ./installcredprovider.ps1 
+./installcredprovider.ps1 -AddNetfx   
+remove-item .\installcredprovider.ps1
+
+
+wget https://dot.net/v1/dotnet-install.ps1 -o ./dotnet-install.ps1 
+./dotnet-install.ps1 -v 3.1.101  -InstallDir 'C:\Program Files\dotnet' 
+remove-item ./dotnet-install.ps1 
