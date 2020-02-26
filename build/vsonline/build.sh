3b88dea83d9bfad621104199acbaad4e59e49152
Fara Nasiri <Faranak.Nasiri@microsoft.com>
2020-02-26 01:48:53 +0000

    Merged PR 231661: Create Nuget PKGs for services

--- build/vsonline/build.sh
+++ build/vsonline/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/helpers.sh
 
+echo "Build Nuget packages"
+execute "dotnet pack --include-source --include-symbols --no-restore --configuration Release /m /v:m /p:BuildPortal=false /p:BuildOtherServices=false $REPO_ROOT/dirs.proj"
+
 execute "dotnet publish --no-restore --configuration Release /m /v:m /p:BuildPortal=false /p:BuildOtherServices=false $REPO_ROOT/dirs.proj"
