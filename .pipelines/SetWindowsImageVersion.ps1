29218139e3ddda8b26e02e6826a3a544d7df4e51
Rick Krause <rickkr@microsoft.com>
2020-03-06 00:57:25 +0000

    Merged PR 233725: add ability to update versions for server images
    Added ability to update versions for server images. This is a companion PR for !233691.
    
    Related work items: #1074362

--- .pipelines/SetWindowsImageVersion.ps1
+++ .pipelines/SetWindowsImageVersion.ps1
@@ -3,10 +3,13 @@ param(
     [string]$AppSettingsFile,
     [Parameter(Mandatory)]
     [string]$ImageVersion,
-    [switch]$InternalImage
+    [switch]$InternalImage,
+    [ValidateSet("Windows", "32Server", "64Server")]
+    [string]$InternalClass = 'Windows'
 )
 $Content = Get-Content $AppSettingsFile
-$Pattern = if ($InternalImage) { 'NexusInternalWindowsImage' } else { 'NexusWindowsImage' }
+$Pattern = if ($InternalImage) { "NexusInternal$($InternalClass)Image" } else { 'NexusWindowsImage' }
+Write-Host "Updating $Pattern in $AppSettingsFile"
 for ($Index = 0; $Index -lt $Content.Count;) {
     if ($Content[$Index++] -match """imageName"": ""$Pattern"",") {
         $Content[$Index] = $Content[$Index] -replace '(\d{4}\.){2}\d{3}', $ImageVersion
