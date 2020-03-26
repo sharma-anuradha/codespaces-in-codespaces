7a4209cd63c6dfab819cdf582d5a26fd361f1f8b
Nagaraju Kotcharlakota <nkotchar@microsoft.com>
2020-03-26 01:24:37 +0000

    Merged PR 237795: The react application is not getting initialized for GitHub and AzureDevOps
    This started happening after the change !236942 which makes popup windows to not initialize the reactapp
    But, this started happening for GitHub and AzureDevOps tabs as the isInPopupWindow will return true for new tabs as well. And it is hard to differentiate between popup and new tab from within javascript.
    
    Related work items: #1088060

--- src/services/containers/VsClk.Portal.WebSite/ClientApp/src/utils/isInPopupWindow.ts
+++ src/services/containers/VsClk.Portal.WebSite/ClientApp/src/utils/isInPopupWindow.ts
@@ -1,3 +1,11 @@
 export const isInPopupWindow = (): boolean => {
+    if (
+        window.location.href.toLowerCase().includes('github/login') ||
+        window.location.href.toLowerCase().includes('azdev/login')
+    ) {
+        // Both GitHub and AzureDevOps are technically not popups since they are opened in new tab,
+        // but currently we cannot differentiate between a popup and tabs.
+        return false;
+    }
     return !!(window.opener && window.opener !== window);
 };
