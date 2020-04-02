649e6aaf5fcd01e8f7e7d2c13118e2b0e7a5b3e0
Osvaldo Ortega Valencia <osortega@microsoft.com>
2020-04-02 17:59:28 +0000

    Merged PR 238901: Splash screen context when environment suspends
    Splash screen context when environment suspends
    
    ![image.png](https://devdiv.visualstudio.com/579bc0c8-404b-48fd-a86e-9d65b001f1bf/_apis/git/repositories/b262268f-bbd7-4d01-b7dc-b03666e77189/pullRequests/238901/attachments/image.png)
    
    ![image.png](https://devdiv.visualstudio.com/579bc0c8-404b-48fd-a86e-9d65b001f1bf/_apis/git/repositories/b262268f-bbd7-4d01-b7dc-b03666e77189/pullRequests/238901/attachments/image%20(2).png)

--- src/services/containers/VsClk.Portal.WebSite/ClientApp/src/components/workbenchSplashScreen/workbenchSplashScreen.tsx
+++ src/services/containers/VsClk.Portal.WebSite/ClientApp/src/components/workbenchSplashScreen/workbenchSplashScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 import { PrimaryButton, Stack, Text } from 'office-ui-fabric-react';
-import { PortalLayout } from '../portalLayout/portalLayout';
 import { BackToEnvironmentsLink } from '../back-to-environments/back-to-environments';
 import { VSOSplashScreen, IConnectionAdapter } from '@vs/vso-splash-screen'
 import { stateToDisplayName } from '../../utils/environmentUtils';
@@ -11,7 +10,14 @@ import classnames from 'classnames';
 import { isHostedOnGithub } from '../../utils/isHostedOnGithub';
 
 interface IRenderSplashScreenProps {
-    message: string;
+    isOnGithub: boolean;
+}
+
+interface IConnectSplashScreen {
+    isOnGithub: boolean;
+    title: string;
+    buttonText: string;
+    onConnect?:() => void;
 }
 
 class CommunicationProvider implements IConnectionAdapter {
@@ -32,62 +38,83 @@ class CommunicationProvider implements IConnectionAdapter {
 }
 
 export const RenderSplashScreen: React.FunctionComponent<IRenderSplashScreenProps> = (props) => {
+    const mainClass = classnames('vsonline-splash-screen-main', {'is-github': props.isOnGithub});
     return (
-        <PortalLayout>
-            <Stack
-                horizontalAlign='center'
-                verticalFill
-                verticalAlign='center'
-                tokens={{ childrenGap: '20' }}
-            >
-                <Stack.Item>
-                    <Text>{props.message}</Text>
-                </Stack.Item>
-                <Stack.Item>{props.children}</Stack.Item>
-                <Stack.Item>
-                    <BackToEnvironmentsLink />
-                </Stack.Item>
-            </Stack>
-        </PortalLayout>
+        <div className={mainClass}>
+            <div className="vsonline-splash-screen-extensions-pane"></div>
+            <div className='vsonline-splash-screen-tree-pane'></div>
+            <div className="vsonline-splash-screen-editor">
+                <div className='vsonline-splash-screen-titlebar'>
+                    <div className='vsonline-splash-screen-titlebar-tab'></div>
+                </div>
+                <div className="vsonline-splash-screen-body">
+                    {props.children}
+                </div>
+            </div>
+        </div>
     );
 };
+
+export const ConnectSplashScreen: React.FunctionComponent<IConnectSplashScreen> = (props) => {
+    return (
+        <RenderSplashScreen isOnGithub={props.isOnGithub}>
+            <Stack
+                    horizontalAlign='center'
+                    verticalFill
+                    verticalAlign='center'
+                    tokens={{ childrenGap: '20' }}>
+                <Stack.Item>
+                    <Text className='text-color'>{props.title}</Text>
+                </Stack.Item>
+                <Stack.Item>
+                    <PrimaryButton onClick={props.onConnect}>Connect</PrimaryButton></Stack.Item>
+                {!props.isOnGithub
+                ? <Stack.Item>
+                    <BackToEnvironmentsLink />
+                  </Stack.Item>
+                : <></>}
+            </Stack>
+        </RenderSplashScreen>
+    );
+}
 
 export const WorkbenchSplashScreen: React.FC<IWorkbenchSplashScreenProps> = (props: IWorkbenchSplashScreenProps) => {
     const connection = React.useMemo(() => { return new CommunicationProvider() }, []);
     const { showPrompt, environment, connectError, onRetry, onConnect } = props;
     const { friendlyName } = environment;
     const isOnGithub = isHostedOnGithub();
-    const mainClass = classnames('vsonline-splash-screen-main', {'is-github': isOnGithub});
+
     if (connectError !== null) {
         return (
-            <RenderSplashScreen message={`Connecting to environment ${friendlyName} failed. ${connectError}`}>
-                <PrimaryButton onClick={onRetry}>Retry</PrimaryButton>
-            </RenderSplashScreen>
+            <ConnectSplashScreen
+                isOnGithub={isOnGithub}
+                title={`Connecting to environment ${friendlyName} failed. ${connectError}`}
+                buttonText='Retry'
+                onConnect={onRetry}
+                >
+            </ConnectSplashScreen>
         );
     }
     
-        const envState = stateToDisplayName(environment!.state).toLocaleLowerCase();
-        if (showPrompt) {
-            return (
-                <RenderSplashScreen message={`Environment "${friendlyName}" is ${envState}.`}>
-                    <PrimaryButton onClick={onConnect}>Connect</PrimaryButton>
+    const envState = stateToDisplayName(environment!.state).toLocaleLowerCase();
+    if (showPrompt) {
+        const containerNaming = isOnGithub ? 'Workspace' : 'Environment';
+        const title = `${containerNaming} "${friendlyName}" is ${envState}.`
+        return (
+            <ConnectSplashScreen
+                isOnGithub={isOnGithub}
+                title={title}
+                buttonText='Connect'
+                onConnect={onConnect}
+                >
+            </ConnectSplashScreen>
+        );
+    } else {
+        return (
+                <RenderSplashScreen isOnGithub={isOnGithub}>
+                    <VSOSplashScreen connection={connection} github={isOnGithub}></VSOSplashScreen>
                 </RenderSplashScreen>
-            );
-        } else {
-            return (
-                    <div className={mainClass}>
-                        <div className="vsonline-splash-screen-extensions-pane"></div>
-                        <div className='vsonline-splash-screen-tree-pane'></div>
-                        <div className="vsonline-splash-screen-editor">
-                            <div className='vsonline-splash-screen-titlebar'>
-                                <div className='vsonline-splash-screen-titlebar-tab'></div>
-                            </div>
-                            <div className="vsonline-splash-screen-body">
-                                <VSOSplashScreen connection={connection} github={isOnGithub}></VSOSplashScreen>
-                            </div>
-                        </div>
-                    </div>
-                    
-            );
-        }
+        );
+    }
+
 }
