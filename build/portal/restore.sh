057faa51becf939317cbf93459aea81127cd320b
Peter Lisy <pelisy@microsoft.com>
2020-05-12 18:37:17 +0000

    Merged PR 248177: fix: Update portal Dockerfile to take files from current folder
    fix: Update portal Dockerfile to take files from current folder instead of publish.

--- build/portal/restore.sh
+++ build/portal/restore.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../common/helpers.sh
 
-execute "export DEBIAN_FRONTEND=noninteractive"
+export DEBIAN_FRONTEND=noninteractive
+
 execute "curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -"
 execute "sudo apt-get install -y nodejs"
-execute "npm i -gf yarn"
+execute "npm i -gf --loglevel error yarn"
 
 pushd "$REPO_ROOT/src/Portal/PortalWebsite/Src/Website/"
 execute "yarn --network-timeout 1000000 --frozen-lockfile"
