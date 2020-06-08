1a5c92d7a956e9768539e062e55f4dcac460a14f
Derek Bekoe <debekoe@microsoft.com>
2020-06-08 21:03:44 +0000

    Merged PR 252139: Update VSO RP API versions used by vso-portal
    Update VSO RP API versions used by vso-portal
    
    2019-07-01 -> 2020-05-26
    
    Related to: https://devdiv.visualstudio.com/DevDiv/_git/Cascade/pullrequest/252135

--- src/Portal/PortalWebsite/Src/Website/packages/website/src/utils/getAzureManagementApiVersion.ts
+++ src/Portal/PortalWebsite/Src/Website/packages/website/src/utils/getAzureManagementApiVersion.ts
@@ -1,21 +1,21 @@
 /**
  * API Version	Endpoint URL
- * 2019-07-01-preview	            online.visualstudio.com/api/v1
- * 2019-07-01-beta	                online-ppe.vsengsaas.visualstudio.com/api/v1
- * 2019-07-01-alpha	                online.dev.vsengsaas.visualstudio.com/api/v1
- * 2019-07-01-privatepreview	    canary.online.visualstudio.com/api/v1
+ * 2020-05-26-preview	            online.visualstudio.com/api/v1
+ * 2020-05-26-beta	                online-ppe.vsengsaas.visualstudio.com/api/v1
+ * 2020-05-26-alpha	                online.dev.vsengsaas.visualstudio.com/api/v1
+ * 2020-05-26-privatepreview	    canary.online.visualstudio.com/api/v1
  */
 export const getAzureManagementApiVersion = (): string => {
     const baseURL = window.location.href.split('/')[2];
     let apiVersion;
     if (baseURL.includes('dev')) {
-        apiVersion = '2019-07-01-alpha';
+        apiVersion = '2020-05-26-alpha';
     } else if (baseURL.includes('ppe')) {
-        apiVersion = '2019-07-01-beta';
+        apiVersion = '2020-05-26-beta';
     } else if (baseURL.includes('canary')) {
-        apiVersion = '2019-07-01-privatepreview';
+        apiVersion = '2020-05-26-privatepreview';
     } else {
-        apiVersion = '2019-07-01-preview';
+        apiVersion = '2020-05-26-preview';
     }
     return apiVersion;
 };
