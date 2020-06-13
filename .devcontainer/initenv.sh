07bc48d31d1b693d99befcf18dc2c9414d6bda0c
Eljo George <elpadann@microsoft.com>
2020-06-13 03:33:01 +0000

    Merged PR 254358: Enable portal development from Codespaces with .npmrc automation using Secret management
    - Automatically creates user npmrc file (`~\.npmrc`) with the feeds necessary for Codespace portal development (done by postcreatecommand)
    - References PAT from an environment variable, which can be set using the Secret management feature
    
    Once this is checked in, developers just need to create an Environment Variable using the new secret management feature.
    Env var name: DEVDIV_PAT
    Value: PAT
    Filters: Something like Git Repository = `*/vsclk-core`  so that env var will get set during env creation/resume.
    
    **Latest Update**
    - User can set the PAT as it is in the secret DEVDIV_PAT
    - Post create command will create another environment variable named DEVDIV_PAT_BASE64 by encoding DEVDIV_PAT
    - This variable is set during creating and resumes, and updates made to secrets are automatically reflected
    
    Related work items: #1066807

--- .devcontainer/initenv.sh
+++ .devcontainer/initenv.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Set env var with a base64 encoded DEVDIV_PAT for npmrc to consume
+echo export DEVDIV_PAT_BASE64="\$(printf "%s" \"\${DEVDIV_PAT}\" | base64)" >> ~/.bashrc
+source ~/.bashrc
+
+# Create user .npmrc file
+cp .devcontainer/user_npmrc ~/.npmrc
+
 # Install .NET Core 3.1.200 SDK. Need to use the user-local install, not the package manager install.
 # TODO: Remove once 3.1.200 is included in the Oryx base image
 wget -q https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
