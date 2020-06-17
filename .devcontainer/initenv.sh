4ebd427ceba48ff67aa1fe265ddd8e6152003468
Eljo George <elpadann@microsoft.com>
2020-06-17 22:04:24 +0000

    Merged PR 255601: Support nuget feed credentials via secret management for VSCLK_CORE
    Inject credentials required by Nuget feeds for the vsclk-core project.
    
    Feed URLs with credential stub is stored in a JSON file. Post create command will substitute the credentials in this file with the PAT, and set the JSON content to an environment variable named `VSS_NUGET_EXTERNAL_FEED_ENDPOINTS`. This environment variable is consumed by the credential manager as per the [documentation here](https://github.com/microsoft/artifacts-credprovider#unattended-build-agents)
    
    - How to use it?
    Create the following env var from the Portal Settings page. THAT'S ALL.
    `DEVDIV_PAT`  (Value= create a PAT here => https://devdiv.visualstudio.com/_usersSettings/tokens)
    
    - Caveat
    `dotnet restore` command would work only after you suspend and resume after the initial creation. This is likely related to [this dotnet issue #4053](https://github.com/dotnet/core/issues/4053) that @<John Keech> has reported a while ago

--- .devcontainer/initenv.sh
+++ .devcontainer/initenv.sh
@@ -2,6 +2,12 @@
 
 # Set env var with a base64 encoded DEVDIV_PAT for npmrc to consume
 echo export DEVDIV_PAT_BASE64="\$(printf "%s" \"\${DEVDIV_PAT}\" | base64)" >> ~/.bashrc
+
+# Interpolate credential env vars inside nuget_feed_auth.json and set it to VSS_NUGET_EXTERNAL_FEED_ENDPOINTS, for credential manager to consume
+# dotnet restore command will use this credentials via credential manager (https://github.com/microsoft/artifacts-credprovider#environment-variables)
+cp .devcontainer/nuget_feed_auth.json ~/.nuget.json
+echo export VSS_NUGET_EXTERNAL_FEED_ENDPOINTS=\`eval \"printf %s \''$(cat ~/.nuget.json)'\'\"\` >> ~/.bashrc
+
 source ~/.bashrc
 
 # Create user .npmrc file
