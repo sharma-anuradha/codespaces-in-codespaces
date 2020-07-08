c952db7dd32a2b8387c90d1cc82260f03108b877
Derek Bekoe <debekoe@microsoft.com>
2020-07-08 19:45:13 +0000

    Merged PR 259822: Update Linux VM Debian Package version
    Update Linux VM Debian Package version
    
    libnss3_3.35-2ubuntu2.8_amd64.deb -> libnss3_3.35-2ubuntu2.9_amd64.deb

--- src/Resources/ComputeVirtualMachineProvider/Src/ComputeVirtualMachineProvider/Templates/Linux/vm_init.sh
+++ src/Resources/ComputeVirtualMachineProvider/Src/ComputeVirtualMachineProvider/Templates/Linux/vm_init.sh
@@ -39,7 +39,7 @@ declare -a DebPackagesArray=(
     "http://azure.archive.ubuntu.com/ubuntu/pool/universe/p/pigz/pigz_2.4-1_amd64.deb"
     # chrony=3.2-4ubuntu4.4 (as well as dependencies: libnss, libnspr4)
     "http://azure.archive.ubuntu.com/ubuntu/pool/main/c/chrony/chrony_3.2-4ubuntu4.4_amd64.deb"
-    "http://azure.archive.ubuntu.com/ubuntu/pool/main/n/nss/libnss3_3.35-2ubuntu2.8_amd64.deb"
+    "http://azure.archive.ubuntu.com/ubuntu/pool/main/n/nss/libnss3_3.35-2ubuntu2.9_amd64.deb"
     "http://azure.archive.ubuntu.com/ubuntu/pool/main/n/nspr/libnspr4_4.18-1ubuntu1_amd64.deb"
     # unzip
     "http://azure.archive.ubuntu.com/ubuntu/pool/main/u/unzip/unzip_6.0-21ubuntu1_amd64.deb"
