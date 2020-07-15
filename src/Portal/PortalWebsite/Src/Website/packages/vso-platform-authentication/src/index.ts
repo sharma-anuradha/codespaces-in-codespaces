bf2f717e718a2f466ab589764eb3fd059a0eddca
Taiyo Sogawa <tasogawa@microsoft.com>
2020-07-15 18:47:27 +0000

    Merged PR 260803: Redirects to codespace if provided via query param

--- src/Portal/PortalWebsite/Src/Website/packages/vso-platform-authentication/src/index.ts
+++ src/Portal/PortalWebsite/Src/Website/packages/vso-platform-authentication/src/index.ts
@@ -9,7 +9,7 @@ import {
     /**
      * TODO: render error page for all errors and redirect to the correct place.
      */
-
+    
     const dataEl = document.querySelector('#js-partner-info');
     if (!dataEl) {
         throw new Error('No data element found.');
@@ -19,20 +19,29 @@ import {
     if (!textContent) {
         throw new Error('No data element found.');
     }
-
+    
     try {
         const data = JSON.parse(atob(textContent));
-
+        
         const keys = await createKeys(data.codespaceToken);
         if (!keys.length) {
             throw new Error('Cannot create encryption keys.');
         }
 
         setKeychainKeys(keys);
-
+        
         await localStorageKeychain.set(PARTNER_INFO_KEYCHAIN_KEY, JSON.stringify(data));
+        
+        const params = new URLSearchParams(window.location.search);
 
-        location.href = '/';
+        const redirectParam = params.get('redirect') || '/';
+        const relativeRedirect = new URL(redirectParam, location.href).pathname;
+
+        // if `redirect` param was set to non-empty string(multiple spaces or non-path string),
+        // the parsing logic above will result the relativeRedirect to be the current path
+        location.href = (relativeRedirect === location.pathname)
+            ? '/'
+            : relativeRedirect;
     } catch (e) {
         throw e;
     }
