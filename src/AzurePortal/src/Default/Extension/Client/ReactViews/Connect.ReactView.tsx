b19cb21840b5b72bcaccafa945ac368c3c31c3c1
Taiyo Sogawa <tasogawa@microsoft.com>
2020-07-23 23:56:18 +0000

    Merged PR 263077: Connect scans for the appropriate Cascade Token if no PlanID supplied
    ## What's new?
    If no PlanId is supplied to the connect page, we will scan all subs and all plans for the appropriate Cascade token.
    
    We need this for the scenario in which the browser does not have the appropriate cookie and the user enters a URL to connect (ex. a port forwarding URL).
    
    This is a temporary measure until we have a tenant-level API for mapping (ARM token, codespace ID) -> (plan ID)
    
    ## Limitations
    The ajax() function in a React blade doesn't have any clear way of checking the response status (it works fine in a non-react blade) and the `fetch` API doesn't when running the extension locally (due to CORS limitations). So it is possible that we receive a 429 or something but fail to retry. However, a 429 from `/environments/{id}` seems unlikely because the limit is in the 100's/minute.

--- src/AzurePortal/src/Default/Extension/Client/ReactViews/Connect.ReactView.tsx
+++ src/AzurePortal/src/Default/Extension/Client/ReactViews/Connect.ReactView.tsx
@@ -4,21 +4,39 @@ import { Fabric } from '@fluentui/react/lib/Fabric';
 import * as ReactView from '@microsoft/azureportal-reactview/ReactView';
 import * as Ajax from '@microsoft/azureportal-reactview/Ajax';
 import { versionId } from '@microsoft/azureportal-reactview/major-version/1';
+import { DefaultButton, PrimaryButton } from "@fluentui/react/lib/Button";
+import { Spinner } from '@fluentui/react/lib/Spinner';
+import {
+    MessageBar,
+    MessageBarType,
+  } from '@fluentui/react/lib/MessageBar';
 
 interface ComponentState {
     sessionId: string;
+    isConnecting: boolean;
+    errorMessage: string;
     connectParams: ConnectParams;
 }
 
 interface ConnectParams {
     parameters: {
-        planId: string;
         codespaceId: string;
+        planId?: string;
         codespacesEndpoint?: string;
         armApiVersion?: string;
         encryptedGitToken?: string;
     };
 }
+
+type ArmList = {
+    value: { id: string }[]
+};
+
+type TokenSearchStatus = {
+    allPlansFound: boolean;
+    allTokensFound: boolean;
+    matchingTokenFound: boolean;
+};
 
 /**
  * This is a view that does not have a corresponding model but instead keeps all of it's business login view side.
@@ -33,6 +51,12 @@ export class ModelFree extends React.Component<ConnectParams, ComponentState> {
     public constructor(props: ConnectParams) {
         super(props);
         Az.setTitle('Opening Codespace...');
+        this.state = {
+            sessionId: '',
+            errorMessage: '',
+            isConnecting: true,
+            connectParams: props,
+        };
         Az.getSessionId().then((sessionId) => this.setState({ sessionId }));
         this.connectAndClose = this.connectAndClose.bind(this);
         this.closeBlade = this.closeBlade.bind(this);
@@ -41,18 +65,35 @@ export class ModelFree extends React.Component<ConnectParams, ComponentState> {
     public render() {
         return (
             <Fabric>
-                <div>
-                    <h2>Not seeing the Codespace open in a new tab?</h2>
-                    To prevent this from happening again, disable your pop-up blocker for this site.
-                    <br />
-                    <br />
-                    <button onClick={this.connectAndClose}>Connect to Codespace</button>
-                    <button onClick={this.closeBlade}>Cancel</button>
-                </div>
+                {this.state.errorMessage &&
+                    <MessageBar messageBarType={MessageBarType.error} isMultiline={false}>
+                        {this.state.errorMessage}
+                    </MessageBar>
+                }
+                {this.state.isConnecting &&
+                    <Spinner label="Opening New Tab"/>
+                }
+                {!this.state.isConnecting && 
+                    <div className="ms-Grid" dir="ltr" style={{maxWidth: '500px'}}>
+                        <div className="ms-Grid-row">
+                            <div className="ms-Grid-col ms-sm12 ms-lg6">
+                                <h2>Not seeing the Codespace open in a new tab?</h2>
+                                To prevent this from happening again, disable your pop-up blocker for this site.
+                            </div>
+                        </div>
+                        
+                        <div className="ms-Grid-row" style={{marginTop: '30px'}}>
+                            <div className="ms-Grid-col ms-sm12 ms-lg6">
+                                <DefaultButton onClick={this.closeBlade} style={{float: 'left'}}>Cancel</DefaultButton>
+                                <PrimaryButton onClick={this.connectAndClose} style={{float: 'right'}}>Connect to Codespace</PrimaryButton>
+                            </div>
+                        </div>
+                    </div>
+                }
             </Fabric>
         );
     }
-
+    
     public componentDidMount() {
         this.connect();
         setTimeout(() => this.closeBlade(), 20 * 1000);
@@ -67,84 +108,184 @@ export class ModelFree extends React.Component<ConnectParams, ComponentState> {
     }
 
     private async connect() {
-        let token: string;
+        this.setState({errorMessage: undefined, isConnecting: true});
+        let githubToken: string;
         if (this.props.parameters.encryptedGitToken) {
-            token = await Ajax.ajax({
+            githubToken = await Ajax.ajax({
                 type: 'POST',
                 crossDomain: true,
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                uri: `${this.getCodespacesEndpoint()}github-auth/decrypt-token`,
+                uri: this.formatCodespacesUri('github-auth/decrypt-token'),
                 data: JSON.stringify({
                     value: this.props.parameters.encryptedGitToken,
                 }),
             });
         }
 
+        const accessToken = await this.getCascadeToken().catch((errorMessage) => {
+            this.setState({errorMessage, isConnecting: false});
+            throw errorMessage;
+        });
+        const authUri = this.formatCodespacesUri(`platform-authentication?redirect=workspace/${this.props.parameters.codespaceId}`);
+        const form = document.createElement('form');
+        form.setAttribute('action', authUri);
+        form.setAttribute('method', 'POST');
+        form.setAttribute('target', '_blank');
+
+        const partnerInfo = JSON.stringify({
+            partnerName: 'azureportal',
+            managementPortalUrl: 'https://portal.azure.com',
+            codespaceToken: accessToken,
+            codespaceId: this.props.parameters.codespaceId,
+            credentials: [
+                {
+                    expiration: 10000000000000,
+                    token: githubToken,
+                    host: 'github.com',
+                    path: '/',
+                },
+            ],
+            vscodeSettings: {},
+        });
+
+        // set the Codespace token
+        const codespaceTokenInput = document.createElement('input');
+        codespaceTokenInput.name = 'codespaceToken';
+        codespaceTokenInput.value = accessToken;
+
+        // set the partner info
+        const partnerInfoInput = document.createElement('input');
+        partnerInfoInput.name = 'partnerInfo';
+        partnerInfoInput.value = partnerInfo;
+
+        // append the form to the DOM
+        form.appendChild(codespaceTokenInput);
+        form.appendChild(partnerInfoInput);
+        document.body.append(form);
+
+        // submit the form to initiate the top-level HTTP POST
+        form.submit();
+        this.setState({isConnecting: false});
+    }
+
+    private getSubscriptions(): Promise<string[]> {
         return Ajax.ajax({
             setAuthorizationHeader: true,
-            type: 'POST',
-            uri: this.getArmUri(),
-        }).then(({ accessToken }) => {
-            const form = document.createElement('form');
-            form.setAttribute('action', this.getCodespacesUri());
-            form.setAttribute('method', 'POST');
-            form.setAttribute('target', '_blank');
+            type: 'GET',
+            uri: this.formatArmUri('subscriptions', '2020-01-01'),
+        }).then(
+            (subs: ArmList) => subs.value.map(({id}) => id)
+        );
+    }
 
-            const partnerInfo = JSON.stringify({
-                partnerName: 'azureportal',
-                managementPortalUrl: 'https://portal.azure.com',
-                codespaceToken: accessToken,
-                codespaceId: this.props.parameters.codespaceId,
-                credentials: [
-                    {
-                        expiration: 10000000000000,
-                        token: token,
-                        host: 'github.com',
-                        path: '/',
-                    },
-                ],
-                vscodeSettings: {},
+    private async getCascadeToken(): Promise<string> {
+        if (this.props.parameters.planId) {
+            const { accessToken } = await Ajax.ajax({
+                setAuthorizationHeader: true,
+                type: 'POST',
+                uri: this.formatArmUri(`${this.props.parameters.planId}/writeCodespaces`),
             });
+            return accessToken;
+        }
 
-            // set the Codespace token
-            const codespaceTokenInput = document.createElement('input');
-            codespaceTokenInput.name = 'codespaceToken';
-            codespaceTokenInput.value = accessToken;
+        const searchStatus: TokenSearchStatus = { allPlansFound: false, allTokensFound: false, matchingTokenFound: false };
+        const subs = await this.getSubscriptions();
+        const plans: string[] = [];
+        const cascadeTokens: string[] = [];
 
-            // set the partner info
-            const partnerInfoInput = document.createElement('input');
-            partnerInfoInput.name = 'partnerInfo';
-            partnerInfoInput.value = partnerInfo;
+        this.findPlans(subs, plans, searchStatus); // fire and forget
+        this.findCascadeTokens(plans, cascadeTokens, searchStatus); // fire and forget
+        return this.findMatchingCascadeToken(cascadeTokens, searchStatus);
+    }
+    
+    private async findPlans(subscriptions: string[], plans: string[], searchStatus: TokenSearchStatus): Promise<void> {
+        while (!searchStatus.matchingTokenFound) {
+            if (subscriptions.length === 0) {
+                break;
+            }
+            const batchSize = Math.min(20, subscriptions.length);
+            const subBatch = subscriptions.splice(0, batchSize);
+            const batchRequests: Common.Ajax.BatchRequest[] = subBatch.map((sub) => ({
+                httpMethod: 'GET',
+                uri: this.formatArmUri(`${sub}/providers/Microsoft.Codespaces/plans`)
+            }));
+            const batchResponse = await Ajax.batchMultiple({ batchRequests });
+            batchResponse.responses.forEach(
+                (element: {content: ArmList}) => element.content.value.forEach((plan) => plans.push(plan.id))
+            );
+        }
+        searchStatus.allPlansFound = true;
+    }
+    
+    private async findCascadeTokens(plans: string[], cascadeTokens: string[], searchStatus: TokenSearchStatus): Promise<void> {
+        while (!searchStatus.matchingTokenFound) {
+            if (plans.length === 0) {
+                if (searchStatus.allPlansFound) {
+                    break;
+                }
+                await new Promise((resolve) => setTimeout(resolve, 500));
+                continue;
+            }
+            const batchSize = Math.min(20, plans.length);   
+            const planBatch = plans.splice(0, batchSize);
+            const batchRequests: Common.Ajax.BatchRequest[] = planBatch.map((planId) => ({
+                httpMethod: 'POST',
+                uri: this.formatArmUri(`${planId}/writeCodespaces`)
+            }));
+            const batchResponse = await Ajax.batchMultiple({ batchRequests });
+            batchResponse.responses.forEach((r) => cascadeTokens.push(r.content.accessToken));
+        }
+        searchStatus.allTokensFound = true;
+    }
+    
+    private async findMatchingCascadeToken(cascadeTokens: string[], searchStatus: TokenSearchStatus): Promise<string> {
+        return new Promise(async (resolve, reject) => {
+            // Helps us determine if there are checks in progress
+            var inProgressCount = 0;
 
-            // append the form to the DOM
-            form.appendChild(codespaceTokenInput);
-            form.appendChild(partnerInfoInput);
-            document.body.append(form);
+            while(!searchStatus.matchingTokenFound) {
+                if (cascadeTokens.length === 0) {
+                    if (searchStatus.allTokensFound && inProgressCount === 0) {
+                        reject(`Could not determine plan for Codespace ${this.props.parameters.codespaceId}`);
+                        return;
+                    }
+                    await new Promise((resolve) => setTimeout(resolve, 500));
+                    continue;
+                }
 
-            // submit the form to initiate the top-level HTTP POST
-            form.submit();
+                inProgressCount++;
+                const cascadeToken = cascadeTokens.pop();
+                const uri = this.formatCodespacesUri(`api/v1/environments/${this.props.parameters.codespaceId}`);
+
+                // fire-and-forget
+                Ajax.ajax({
+                    type: 'GET',
+                    crossDomain: true,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${cascadeToken}`
+                    },
+                    uri,
+                }).then(() => {
+                    searchStatus.matchingTokenFound = true;
+                    resolve(cascadeToken);
+                }).catch(() => inProgressCount--);
+            }
         });
     }
 
-    private getArmUri(): string {
-        const apiVersion = this.props.parameters.armApiVersion || '2019-07-01-preview';
-        return `https://management.azure.com${this.props.parameters.planId}/writeCodespaces?api-version=${apiVersion}`;
+    private formatCodespacesUri(path: string = ''): string {
+        const endpoint = this.props.parameters.codespacesEndpoint || 'https://online.visualstudio.com/';
+        return new URL(path, endpoint).href;
     }
 
-    private getCodespacesEndpoint() {
-        var codespacesEndpoint =
-            this.props.parameters.codespacesEndpoint || 'https://online.visualstudio.com/';
-        if (codespacesEndpoint.substring(codespacesEndpoint.length - 1) !== '/') {
-            codespacesEndpoint += '/';
-        }
-        return codespacesEndpoint;
-    }
-
-    private getCodespacesUri(): string {
-        return `${this.getCodespacesEndpoint()}platform-authentication?redirect=workspace/${
-            this.props.parameters.codespaceId
-        }`;
+    private formatArmUri(path: string = '', apiVersion: string = ''): string {
+        apiVersion = apiVersion || this.props.parameters.armApiVersion || '2020-06-16';
+        const endpoint = 'https://management.azure.com/';
+        const url = new URL(path, endpoint);
+        url.searchParams.set('api-version', apiVersion);
+        return url.href;
     }
 }
