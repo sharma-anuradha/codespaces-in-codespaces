a821ce081af3e00e39b7b9529312eb7492659d55
Project Collection Build Service (devdiv) <Project Collection Build Service (devdiv)>
2020-09-18 10:23:28 +0000

    Insert VSCode commit update Payload into master

--- src/Portal/PortalWebsite/Src/Website/public/amdconfig.js
+++ src/Portal/PortalWebsite/Src/Website/public/amdconfig.js
@@ -16,7 +16,7 @@ if (paramsFeatureSet === insiderFlag) {
 }
 
 const commits = {
-    insider: 'd1f267742169584db010716d92addf50d4cd8139',
+    insider: '403a6895f401c397933ec5c4f7d89432a33509b6',
     stable: '58bb7b2331731bf72587010e943852e13e6fd3cf',
 };
 
