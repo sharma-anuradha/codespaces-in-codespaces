25a1c12fc74ffbde16f87d6a4e0b9036c0f98446
Vincent Dondain <vidondai@microsoft.com>
2020-09-18 18:28:29 +0000

    Merged PR 275685: Add VSCode assets version query param
    This ensures assets and resources are properly recreated. Otherwise the service worker's cache could store incorrect content.

--- src/Portal/PortalWebsite/Src/Website/public/amdconfig.js
+++ src/Portal/PortalWebsite/Src/Website/public/amdconfig.js
@@ -29,36 +29,36 @@ self.require = {
         'vscode-textmate': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/vscode-textmate/release/main`,
         'onigasm-umd': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/onigasm-umd/release/main`,
         'vscode-oniguruma': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/vscode-oniguruma/release/main`,
-        'xterm': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/xterm/lib/xterm.js`,
-        'xterm-addon-search': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/xterm-addon-search/lib/xterm-addon-search.js`,
-        'xterm-addon-web-links': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/xterm-addon-web-links/lib/xterm-addon-web-links.js`,
-        'semver-umd': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/semver-umd/lib/semver-umd.js`,
-        'iconv-lite-umd': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/iconv-lite-umd/lib/iconv-lite-umd.js`,
-        'jschardet': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/jschardet/dist/jschardet.min.js`,
+        'xterm': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/xterm/lib/xterm.js?v=2`,
+        'xterm-addon-search': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/xterm-addon-search/lib/xterm-addon-search.js?v=2`,
+        'xterm-addon-web-links': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/xterm-addon-web-links/lib/xterm-addon-web-links.js?v=2`,
+        'semver-umd': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/semver-umd/lib/semver-umd.js?v=2`,
+        'iconv-lite-umd': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/iconv-lite-umd/lib/iconv-lite-umd.js?v=2`,
+        'jschardet': `${window.location.origin}/workbench-page/web-standalone/${vscodeCommitId}/node_modules/jschardet/dist/jschardet.min.js?v=2`,
     },
 };
 
 var loaderScript = document.createElement('script');
-loaderScript.src = `/workbench-page/web-standalone/${vscodeCommitId}/out/vs/loader.js`;
+loaderScript.src = `/workbench-page/web-standalone/${vscodeCommitId}/out/vs/loader.js?v=2`;
 document.body.appendChild(loaderScript);
 
 var workbenchNlsScript = document.createElement('script');
-workbenchNlsScript.src = `/workbench-page/web-standalone/${vscodeCommitId}/out/vs/workbench/workbench.web.api.nls.js`;
+workbenchNlsScript.src = `/workbench-page/web-standalone/${vscodeCommitId}/out/vs/workbench/workbench.web.api.nls.js?v=2`;
 document.body.appendChild(workbenchNlsScript);
 
 var workbenchScript = document.createElement('script');
 workbenchScript.defer = true;
-workbenchScript.src = `/workbench-page/web-standalone/${vscodeCommitId}/out/vs/workbench/workbench.web.api.js`;
+workbenchScript.src = `/workbench-page/web-standalone/${vscodeCommitId}/out/vs/workbench/workbench.web.api.js?v=2`;
 document.body.appendChild(workbenchScript);
 
 var cssLink = document.createElement('link');
 cssLink.setAttribute('data-name', 'vs/workbench/workbench.web.api');
 cssLink.rel = 'stylesheet';
 cssLink.type = 'text/css';
-cssLink.href = `/workbench-page/web-standalone/${vscodeCommitId}/out/vs/workbench/workbench.web.api.css`;
+cssLink.href = `/workbench-page/web-standalone/${vscodeCommitId}/out/vs/workbench/workbench.web.api.css?v=2`;
 document.head.appendChild(cssLink);
 
 var serverUmdLink = document.createElement('link');
 serverUmdLink.rel = 'prefetch';
-serverUmdLink.href = `/workbench-page/web-standalone/${vscodeCommitId}/node_modules/semver-umd/lib/semver-umd.js`;
+serverUmdLink.href = `/workbench-page/web-standalone/${vscodeCommitId}/node_modules/semver-umd/lib/semver-umd.js?v=2`;
 document.head.appendChild(serverUmdLink);
