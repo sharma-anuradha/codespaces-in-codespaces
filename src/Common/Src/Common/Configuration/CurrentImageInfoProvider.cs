8535893a30189caeac53401e5e165396837d25e7
Aditya Kumar <kumarad@microsoft.com>
2020-10-16 20:29:02 +0000

    Merged PR 281203: Changing the CurrentImageInfoProvider to use ConfigurationReader
    Fixes: [link to issue] https://github.com/microsoft/vssaas-planning/issues/1036
    
    Description of the Change
    -------------------------
    
    [Provide as many details as possible about what the code is doing, think about folks unfamiliar with the codebase]
    
    This changes CurrentImageInfoProvider to make use of ConfigurationReader API instead of directly making use of SystemConfiguration. This will change the keys that the code uses for lookup.
    
    example of keys -
    1. **setting:vsclk:images-vmagent-vsoagentlinux-name** (global scope, least priority), **setting:vsclk-usw2:images-vmagent-vsoagentlinux-name** (region scope, highest priority)
    2. **setting:vsclk:images-compute-nexusInternalWindowsImage-name** (global scope, least priority), **setting:vsclk-usw2:images-compute-nexusInternalWindowsImage-name** (region scope, highest priority)
    Impact
    ------
    
    [How critical is the fix, think potential hotfix justification]
    This is critical for migration to global BE database for configurations. The [PR 280867](https://devdiv.visualstudio.com/OnlineServices/_git/vsclk-core/pullrequest/280867) writes to the Global BE DB in only the format that could be understood by ConfigurationReader. If we move to Global DB and this change is not present, the code would make queries to global database with keys in old format which won't be present there and hence we would always return the default values.
    
    Rollback Plan
    -------------
    
    [Can we simply revert this commit? If no please detail how to rollback. ⚠️ CRITICAL for DRIs]
    Reverting this change means the code is going to make use of old format of  keys which won't be present in global database. So the DRIs would be required to do one of the following two to make sure they get the desired result:
    1. Revert back and start using regional configuration database - https://devdiv.visualstudio.com/OnlineServices/_git/vsclk-core/pullrequest/278112
    2. Revert and update the [setskuimageversion vso util](https://devdiv.visualstudio.com/OnlineServices/_git/vsclk-core/pullrequest/280867) so that we could add keys in old format to the global database. This would allows us to continue using old key format with global configuration database.

--- src/Common/Src/Common/Configuration/CurrentImageInfoProvider.cs
+++ src/Common/Src/Common/Configuration/CurrentImageInfoProvider.cs
@@ -1,10 +1,11 @@
-﻿// <copyright file="CurrentImageInfoProvider.cs" company="Microsoft">
+// <copyright file="CurrentImageInfoProvider.cs" company="Microsoft">
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 
 using System.Threading.Tasks;
 using Microsoft.VsSaaS.Diagnostics;
 using Microsoft.VsSaaS.Diagnostics.Extensions;
+using Microsoft.VsSaaS.Services.CloudEnvironments.Common.Configuration.KeyGenerator;
 using Microsoft.VsSaaS.Services.CloudEnvironments.Common.Contracts;
 
 namespace Microsoft.VsSaaS.Services.CloudEnvironments.Common.Configuration
@@ -17,14 +18,14 @@ namespace Microsoft.VsSaaS.Services.CloudEnvironments.Common.Configuration
         /// <summary>
         /// Initializes a new instance of the <see cref="CurrentImageInfoProvider"/> class.
         /// </summary>
-        /// <param name="systemConfiguration">The system configuration to use for querying current values.</param>
-        public CurrentImageInfoProvider(ISystemConfiguration systemConfiguration)
+        /// <param name="configurationReader">Configuration reader - to use for querying current values.</param>
+        public CurrentImageInfoProvider(IConfigurationReader configurationReader)
         {
-            Requires.NotNull(systemConfiguration, nameof(systemConfiguration));
-            SystemConfiguration = systemConfiguration;
+            Requires.NotNull(configurationReader, nameof(configurationReader));
+            ConfigurationReader = configurationReader;
         }
 
-        private ISystemConfiguration SystemConfiguration { get; }
+        private IConfigurationReader ConfigurationReader { get; }
 
         /// <inheritdoc/>
         public Task<string> GetImageNameAsync(
@@ -37,8 +38,10 @@ namespace Microsoft.VsSaaS.Services.CloudEnvironments.Common.Configuration
                 $"{LogBaseName}_get_image_name",
                 async (childLogger) =>
                 {
-                    var key = GetConfigurationKey(imageFamilyType, imageFamilyName, "name");
-                    return await SystemConfiguration.GetValueAsync(key, logger, defaultImageName);
+                    // example of keys - setting:vsclk:images-vmagent-vsoagentlinux-name (global scope, least priority), setting:vsclk-usw2:images-vmagent-vsoagentlinux-name (region scope, highest priority)
+                    // setting:vsclk:images-compute-nexusInternalWindowsImage-name (global scope, least priority), setting:vsclk-usw2:images-compute-nexusInternalWindowsImage-name (region scope, highest priority)
+                    var (componentName, configurationName) = GetComponentAndConfigurationName(imageFamilyType, imageFamilyName, "name");
+                    return await ConfigurationReader.ReadSettingAsync(componentName, configurationName, logger.NewChildLogger(), defaultImageName);
                 });
         }
 
@@ -51,27 +54,31 @@ namespace Microsoft.VsSaaS.Services.CloudEnvironments.Common.Configuration
         {
             return logger.OperationScopeAsync(
                 $"{LogBaseName}_get_image_version",
-                (childLogger) =>
+                async (childLogger) =>
                 {
-                    var key = GetConfigurationKey(imageFamilyType, imageFamilyName, "version");
-                    return SystemConfiguration.GetValueAsync(key, logger, defaultImageVersion);
+                    // example of keys - setting:vsclk:images-vmagent-vsoagentlinux-version (global scope, least priority), setting:vsclk-usw2:images-vmagent-vsoagentlinux-version (region scope, highest priority)
+                    // setting:vsclk:images-compute-nexusInternalWindowsImage-version (global scope, least priority), setting:vsclk-usw2:images-compute-nexusInternalWindowsImage-version (region scope, highest priority)
+                    var (componentName, configurationName) = GetComponentAndConfigurationName(imageFamilyType, imageFamilyName, "version");
+                    return await ConfigurationReader.ReadSettingAsync(componentName, configurationName, logger.NewChildLogger(), defaultImageVersion);
                 });
         }
 
         /// <summary>
-        /// Generates the key to use to look up in the configuration database.
+        /// Generates the component name and configuration name to use to look up in the configuration database.
         /// </summary>
         /// <param name="imageFamilyType">The image family type.</param>
         /// <param name="imageFamilyName">The image family name.</param>
         /// <param name="valueType">The value to lookup for the image family.</param>
-        /// <returns>The lookup key.</returns>
-        /// <example>"images:vmagent:vsoagentlinux:name".</example>
-        /// <example>"images:vmagent:vsoagentlinux:version".</example>
-        /// <example>"images:compute:nexusInternalWindowsImage:version".</example>
+        /// <returns>The component name and configuration name to use for lookup.</returns>
+        /// <example>"(images-vmagent, vsoagentlinux-name)".</example>
+        /// <example>"(images-vmagent, vsoagentlinux-version)".</example>
+        /// <example>"(images-compute, nexusInternalWindowsImage-version)".</example>
         /// <remarks>Look at appsettings.images.json for reference about which values can be queried.</remarks>
-        private static string GetConfigurationKey(ImageFamilyType imageFamilyType, string imageFamilyName, string valueType)
+        private static (string, string) GetComponentAndConfigurationName(ImageFamilyType imageFamilyType, string imageFamilyName, string valueType)
         {
-            return $"images:{imageFamilyType.ToString().ToLowerInvariant()}:{imageFamilyName}:{valueType}";
+            var componentName = $"images-{imageFamilyType.ToString().ToLowerInvariant()}";
+            var configurationName = $"{imageFamilyName}-{valueType}";
+            return (componentName, configurationName);
         }
     }
 }
--- src/Common/Src/Common/Configuration/ICurrentImageInfoProvider.cs
+++ src/Common/Src/Common/Configuration/ICurrentImageInfoProvider.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="ICurrentImageInfoProvider.cs" company="Microsoft">
+// <copyright file="ICurrentImageInfoProvider.cs" company="Microsoft">
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 
